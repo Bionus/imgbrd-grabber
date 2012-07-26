20c9a8c4ec063058433183aa641c9bada82d9f40
bio.nus@hotmail.fr <bio.nus@hotmail.fr>
2012-07-26 17:14:15 +0000

    * New tab type: pool

--- Grabber.pro
+++ Grabber.pro
@@ -3,97 +3,103 @@
 ######################################################################
 
 TEMPLATE = app
-TARGET = 
+TARGET =
 DEPENDPATH += . debug includes languages release source ui
 INCLUDEPATH += . includes source
 
 # Input
 HEADERS += includes/aboutwindow.h \
-           includes/addgroupwindow.h \
-           includes/adduniquewindow.h \
-           includes/batchwindow.h \
-           includes/blacklistfix.h \
-           includes/blacklistfix2.h \
-           includes/blacklistfix3.h \
-           includes/customwindow.h \
-           includes/detailswindow.h \
-           includes/emptydirsfix.h \
-           includes/emptydirsfix2.h \
-           includes/favoritewindow.h \
+		   includes/addgroupwindow.h \
+		   includes/adduniquewindow.h \
+		   includes/batchwindow.h \
+		   includes/blacklistfix.h \
+		   includes/blacklistfix2.h \
+		   includes/blacklistfix3.h \
+		   includes/customwindow.h \
+		   includes/detailswindow.h \
+		   includes/emptydirsfix.h \
+		   includes/emptydirsfix2.h \
+		   includes/favoritewindow.h \
 		   includes/conditionwindow.h \
-           includes/functions.h \
-           includes/image.h \
-           includes/imagethread.h \
-           includes/json.h \
-           includes/mainwindow.h \
-           includes/optionswindow.h \
-           includes/page.h \
-           includes/pool.h \
-           includes/QAffiche.h \
-           includes/QBouton.h \
-           includes/searchtab.h \
-           includes/searchwindow.h \
-           includes/sitewindow.h \
-           includes/sourceswindow.h \
-           includes/startwindow.h \
-           includes/tag.h \
-           includes/textedit.h \
+		   includes/functions.h \
+		   includes/image.h \
+		   includes/imagethread.h \
+		   includes/json.h \
+		   includes/mainwindow.h \
+		   includes/optionswindow.h \
+		   includes/page.h \
+		   includes/pool.h \
+		   includes/QAffiche.h \
+		   includes/QBouton.h \
+		   includes/searchtab.h \
+		   includes/searchwindow.h \
+		   includes/sitewindow.h \
+		   includes/sourceswindow.h \
+		   includes/startwindow.h \
+		   includes/tag.h \
+		   includes/textedit.h \
 		   includes/zoomwindow.h \
-           includes/filenamewindow.h
+		   includes/filenamewindow.h \
+	includes/pool.h \
+	includes/pooltab.h \
+	includes/tagtab.h
 FORMS += ui/aboutwindow.ui \
-         ui/adduniquewindow.ui \
-         ui/batchwindow.ui \
-         ui/blacklistfix.ui \
-         ui/blacklistfix2.ui \
-         ui/blacklistfix3.ui \
-         ui/customwindow.ui \
-         ui/detailswindow.ui \
-         ui/emptydirsfix.ui \
-         ui/emptydirsfix2.ui \
-         ui/favoritewindow.ui \
+		 ui/adduniquewindow.ui \
+		 ui/batchwindow.ui \
+		 ui/blacklistfix.ui \
+		 ui/blacklistfix2.ui \
+		 ui/blacklistfix3.ui \
+		 ui/customwindow.ui \
+		 ui/detailswindow.ui \
+		 ui/emptydirsfix.ui \
+		 ui/emptydirsfix2.ui \
+		 ui/favoritewindow.ui \
 		 ui/conditionwindow.ui \
-         ui/mainwindow.ui \
-         ui/optionswindow.ui \
-         ui/searchtab.ui \
-         ui/searchwindow.ui \
-         ui/sitewindow.ui \
-         ui/sourceswindow.ui \
-         ui/startwindow.ui \
+		 ui/mainwindow.ui \
+		 ui/optionswindow.ui \
+		 ui/searchwindow.ui \
+		 ui/sitewindow.ui \
+		 ui/sourceswindow.ui \
+		 ui/startwindow.ui \
 		 ui/zoomwindow.ui \
-         ui/filenamewindow.ui
+		 ui/filenamewindow.ui \
+		 ui/pooltab.ui \
+		 ui/tagtab.ui
 SOURCES += source/aboutwindow.cpp \
-           source/addgroupwindow.cpp \
-           source/adduniquewindow.cpp \
-           source/batchwindow.cpp \
-           source/blacklistfix.cpp \
-           source/blacklistfix2.cpp \
-           source/blacklistfix3.cpp \
-           source/customwindow.cpp \
-           source/detailswindow.cpp \
-           source/emptydirsfix.cpp \
-           source/emptydirsfix2.cpp \
-           source/favoritewindow.cpp \
+		   source/addgroupwindow.cpp \
+		   source/adduniquewindow.cpp \
+		   source/batchwindow.cpp \
+		   source/blacklistfix.cpp \
+		   source/blacklistfix2.cpp \
+		   source/blacklistfix3.cpp \
+		   source/customwindow.cpp \
+		   source/detailswindow.cpp \
+		   source/emptydirsfix.cpp \
+		   source/emptydirsfix2.cpp \
+		   source/favoritewindow.cpp \
 		   source/conditionwindow.cpp \
-           source/functions.cpp \
-           source/image.cpp \
-           source/imagethread.cpp \
-           source/json.cpp \
-           source/main.cpp \
-           source/mainwindow.cpp \
-           source/optionswindow.cpp \
-           source/page.cpp \
-           source/pool.cpp \
-           source/QAffiche.cpp \
-           source/QBouton.cpp \
-           source/searchtab.cpp \
-           source/searchwindow.cpp \
-           source/sitewindow.cpp \
-           source/sourceswindow.cpp \
-           source/startwindow.cpp \
-           source/tag.cpp \
-           source/textedit.cpp \
+		   source/functions.cpp \
+		   source/image.cpp \
+		   source/imagethread.cpp \
+		   source/json.cpp \
+		   source/main.cpp \
+		   source/mainwindow.cpp \
+		   source/optionswindow.cpp \
+		   source/page.cpp \
+		   source/pool.cpp \
+		   source/QAffiche.cpp \
+		   source/QBouton.cpp \
+		   source/searchtab.cpp \
+		   source/searchwindow.cpp \
+		   source/sitewindow.cpp \
+		   source/sourceswindow.cpp \
+		   source/startwindow.cpp \
+		   source/tag.cpp \
+		   source/textedit.cpp \
 		   source/zoomwindow.cpp \
-		   source/filenamewindow.cpp
+		   source/filenamewindow.cpp \
+	source/pooltab.cpp \
+	source/tagtab.cpp
 
 RESOURCES += resources.qrc
 RC_FILE = icon.rc
--- includes/mainwindow.h
+++ includes/mainwindow.h
@@ -5,140 +5,148 @@
 #include <QSettings>
 #include <QProcess>
 #include <QTranslator>
+#include <QSet>
 #include "sourceswindow.h"
 #include "batchwindow.h"
 #include "QAffiche.h"
 #include "QBouton.h"
 #include "textedit.h"
 #include "searchtab.h"
-#include "ui_searchtab.h"
+#include "pooltab.h"
+#include "tagtab.h"
 
 
 
 namespace Ui
 {
-	class mainWindow;
+    class mainWindow;
+    class poolTab;
+    class tagTab;
 }
 
 
 
 class searchTab;
+class poolTab;
+class tagTab;
 
 class mainWindow : public QMainWindow
 {
-	Q_OBJECT
+    Q_OBJECT
 
-	public:
-		explicit mainWindow(QString, QStringList, QMap<QString,QString>);
-		~mainWindow();
-		QMap<QString,QMap<QString,QString> > m_sites;
-		Ui::mainWindow *ui;
+    public:
+        explicit mainWindow(QString, QStringList, QMap<QString,QString>);
+        ~mainWindow();
+        QMap<QString,QMap<QString,QString> > m_sites;
+        Ui::mainWindow *ui;
 
-	public slots:
-		// Log
-		void logShow();
-		void logClear();
-		void logOpen();
-		// Menus
-		void options();
-		void optionsClosed();
-		void aboutAuthor();
-		void saveFolder();
-		void blacklistFix();
-		void emptyDirsFix();
-		// Language
-		void switchTranslator(QTranslator&, const QString&);
-		void loadLanguage(const QString&, bool shutup = false);
-		void changeEvent(QEvent*);
-		// Favorites
-		void loadFavorite(int);
-		void favoriteProperties(int);
-		void updateFavorites(bool dock = false);
-		void updateFavoritesDock();
-		void updateKeepForLater();
-		void viewed();
-		void setFavoriteViewed(QString);
-		void favoritesBack();
-		void checkFavorites();
-		void loadNextFavorite();
-		// Download
-		void web(QString tags = "");
-		void finishedLoading(Page*);
-		void finishedLoadingPreview(Image*);
-		void webZoom(int);
-		// Batch download management
-		void batchClear();
-		void batchClearSel();
-		void batchSel();
-		void batchChange(int);
-		void updateBatchGroups(int, int);
-		void addGroup();
-		void addUnique();
-		void batchAddGroup(const QStringList& values);
-		void batchAddUnique(QMap<QString,QString>);
-		// Batch download
-		void getAll(bool all = true);
-		void getAllImages();
-		void getAllFinishedLoading(Page*);
-		void getAllPerformTags(Image*);
-		void getAllPerformImage(Image*);
-		void getAllProgress(Image*, qint64, qint64);
-		void getAllCancel();
-		void _getAll();
-		// Others
-		void closeEvent(QCloseEvent*);
-		void advanced();
-		void saveAdvanced(sourcesWindow*);
-		void init();
-		void on_buttonSaveLinkList_clicked();
-		void on_buttonLoadLinkList_clicked();
-		bool saveLinkList(QString filename);
-		bool loadLinkList(QString filename);
-		void on_buttonSaveSettings_clicked();
-		void on_buttonInitSettings_clicked();
-		void saveSettings();
-		void on_buttonFolder_clicked();
-		void loadSites();
-		// Tabs
-		int addTab(QString tag = "");
-		void addTabFavorite(int);
-		void updateTabTitle(searchTab*);
-		void tabClosed(searchTab *);
-		void currentTabChanged(int);
-		void closeCurrentTab();
-		void loadTag(QString tag);
-		bool saveTabs(QString);
-		bool loadTabs(QString);
-		void updateTabs();
+    public slots:
+        // Log
+        void logShow();
+        void logClear();
+        void logOpen();
+        // Menus
+        void options();
+        void optionsClosed();
+        void aboutAuthor();
+        void saveFolder();
+        void blacklistFix();
+        void emptyDirsFix();
+        // Language
+        void switchTranslator(QTranslator&, const QString&);
+        void loadLanguage(const QString&, bool shutup = false);
+        void changeEvent(QEvent*);
+        // Favorites
+        void loadFavorite(int);
+        void favoriteProperties(int);
+        void updateFavorites(bool dock = false);
+        void updateFavoritesDock();
+        void updateKeepForLater();
+        void viewed();
+        void setFavoriteViewed(QString);
+        void favoritesBack();
+        void checkFavorites();
+        void loadNextFavorite();
+        // Download
+        void web(QString tags = "");
+        void finishedLoading(Page*);
+        void finishedLoadingPreview(Image*);
+        void webZoom(int);
+        // Batch download management
+        void batchClear();
+        void batchClearSel();
+        void batchSel();
+        void batchChange(int);
+        void updateBatchGroups(int, int);
+        void addGroup();
+        void addUnique();
+        void batchAddGroup(const QStringList& values);
+        void batchAddUnique(QMap<QString,QString>);
+        // Batch download
+        void getAll(bool all = true);
+        void getAllImages();
+        void getAllFinishedLoading(Page*);
+        void getAllPerformTags(Image*);
+        void getAllPerformImage(Image*);
+        void getAllProgress(Image*, qint64, qint64);
+        void getAllCancel();
+        void _getAll();
+        // Others
+        void closeEvent(QCloseEvent*);
+        void advanced();
+        void saveAdvanced(sourcesWindow*);
+        void init();
+        void on_buttonSaveLinkList_clicked();
+        void on_buttonLoadLinkList_clicked();
+        bool saveLinkList(QString filename);
+        bool loadLinkList(QString filename);
+        void on_buttonSaveSettings_clicked();
+        void on_buttonInitSettings_clicked();
+        void saveSettings();
+        void on_buttonFolder_clicked();
+        void loadSites();
+        // Tabs
+        int addTab(QString tag = "");
+        void addTabFavorite(int);
+        void updateTabTitle(searchTab*);
+        void tabClosed(searchTab *);
+        void currentTabChanged(int);
+        void closeCurrentTab();
+        void loadTag(QString tag);
+        bool saveTabs(QString);
+        bool loadTabs(QString);
+        void updateTabs();
 
-	private:
-		int					m_pagemax, m_timezonedecay, m_getAllDownloaded, m_getAllExists, m_getAllIgnored, m_getAll404s, m_getAllErrors, m_getAllCount, m_getAllPageCount, m_getAllBeforeId, m_remainingPics, m_remainingSites, m_countPics, m_currentFav, m_currentFavCount, m_getAllLimit;
-		bool				m_allow, m_must_get_tags, m_loaded, m_getAllRequestExists, m_getAll;
-		QSettings			*m_settings;
-		QProcess			*m_process;
-		QNetworkReply		*m_getAllRequest;
-		batchWindow			*m_progressdialog;
-		QString				m_program, m_currLang, m_currentFavorite;
-		QStringList			m_tags, m_assoc, m_gotMd5;
-		QTranslator			m_translator;
-		QDateTime			m_serverDate, m_loadFavorite;
-		QMap<QString,int>	m_countPage;
-		QList<QStringList>	m_groupBatchs;
-		QList<Image*>		m_getAllRemaining, m_getAllDownloading, m_getAllFailed, m_images;
-		QList<Page*>		m_getAllPages, m_pages;
-		QList<QAffiche*>	m_favoritesCaptions;
-		QList<QBouton*>		m_favoritesImages, m_mergeButtons, m_webPics;
-		QList<searchTab*>	m_tabs;
-		QList<bool>			m_selectedSources;
-		QList<QLabel*>		m_webSites;
-		QMap<QString,double>			m_getAllDownloadingSpeeds;
-		QMap<QString,QString>			m_favorites, m_params;
-		QMap<QString,QStringList>		m_getAllDetails;
-		QMap<QString,QTime*>			m_downloadTime, m_downloadTimeLast;
-		QList<QProgressBar*>			m_progressBars;
-		QList<QNetworkReply*>			m_replies;
-		QList<QMap<QString,QString> >	m_details, m_batchs;
-		QSet<int>						m_batchDownloading;
+    private:
+        int					m_pagemax, m_timezonedecay, m_getAllDownloaded, m_getAllExists, m_getAllIgnored, m_getAll404s, m_getAllErrors, m_getAllCount, m_getAllPageCount, m_getAllBeforeId, m_remainingPics, m_remainingSites, m_countPics, m_currentFav, m_currentFavCount, m_getAllLimit;
+        bool				m_allow, m_must_get_tags, m_loaded, m_getAllRequestExists, m_getAll;
+        QSettings			*m_settings;
+        QProcess			*m_process;
+        QNetworkReply		*m_getAllRequest;
+        batchWindow			*m_progressdialog;
+        QString				m_program, m_currLang, m_currentFavorite;
+        QStringList			m_tags, m_assoc, m_gotMd5;
+        QTranslator			m_translator;
+        QDateTime			m_serverDate, m_loadFavorite;
+        QMap<QString,int>	m_countPage;
+        QList<QStringList>	m_groupBatchs;
+        QList<Image*>		m_getAllRemaining, m_getAllDownloading, m_getAllFailed, m_images;
+        QList<Page*>		m_getAllPages, m_pages;
+        QList<QAffiche*>	m_favoritesCaptions;
+        QList<QBouton*>		m_favoritesImages, m_mergeButtons, m_webPics;
+        QList<searchTab*>	m_tabs;
+        QList<tagTab*>      m_tagTabs;
+        QList<poolTab*>     m_poolTabs;
+        QList<bool>			m_selectedSources;
+        QList<QLabel*>		m_webSites;
+        QMap<QString,double>			m_getAllDownloadingSpeeds;
+        QMap<QString,QString>			m_favorites, m_params;
+        QMap<QString,QStringList>		m_getAllDetails;
+        QMap<QString,QTime*>			m_downloadTime, m_downloadTimeLast;
+        QList<QProgressBar*>			m_progressBars;
+        QList<QNetworkReply*>			m_replies;
+        QList<QMap<QString,QString> >	m_details, m_batchs;
+        QSet<int>						m_batchDownloading;
 };
 
 #endif // MAINWINDOW_H
