dcf2cb112da1fc65ee58bdef7b6c140f2b9f8f5b
bio.nus@hotmail.fr <bio.nus@hotmail.fr@443441d2-a847-b3a4-3658-cc906aa46667>
2012-12-02 00:19:16 +0000

    * SVN cleanup #2

--- trunk/module/defaults/preferences/prefs.js
+++
@@ -1,6 +0,0 @@
-pref("extensions.imgbrdgrabber.boolpref", false);
-pref("extensions.imgbrdgrabber.intpref", 0);
-pref("extensions.imgbrdgrabber.stringpref", "A string");
-
-// https://developer.mozilla.org/en/Localizing_extension_descriptions
-pref("extensions.imgbrd-grabber@bionus.com.description", "chrome://imgbrdgrabber/locale/overlay.properties");
