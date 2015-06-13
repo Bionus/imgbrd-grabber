116565a336a2a150d6d98839d7b36e3eaf183078
Bionus <bio.nus@hotmail.fr>
2015-06-13 04:19:29 +0200

    Changed copy to move command in compiilation on linux

--- README.md
+++ README.md
@@ -62,7 +62,7 @@ sudo apt-get install -qq qt5-qmake qtbase5-dev qtdeclarative5-dev qtscript5-dev 
 sudo apt-get install qt5-default qttools5-dev-tools
 qmake Grabber.pro
 make
-cp Grabber release/Grabber
+mv Grabber release/Grabber
 touch release/settings.ini
 ```
 
--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ apt-get install -qq "qt5-qmake" "qtbase5-dev" "qtdeclarative5-dev" "qtscript5-de
 apt-get install "qt5-default" "qttools5-dev-tools"
 qmake "Grabber.pro"
 make
-cp "Grabber" "release/Grabber"
+mv "Grabber" "release/Grabber"
 touch "release/settings.ini"
 
 echo "Grabber has been compiled in the release directory. To run it, type './release/Grabber'"
