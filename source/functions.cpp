fc6e44bdba4f09452e00d4ee36bd71fddd0329b4
Bionus <bio.nus@hotmail.fr>
2015-10-16 01:41:50 +0200

    Remove useless debug bypass

--- source/functions.cpp
+++ source/functions.cpp
@@ -497,7 +497,7 @@ void log(QString l, Log)
 	//_log.insert(time, (type == Error ? QObject::tr("<b>Erreur :</b> %1").arg(l) : (type == Warning ? QObject::tr("<b>Attention :</b> %1").arg(l) : (type == Notice ? QObject::tr("<b>Notice :</b> %1").arg(l) : l))));
 	//_mainwindow->logShow();
 
-	#ifdef NULL
+	#ifdef QT_DEBUG
 		qDebug() << time.toString("hh:mm:ss.zzz") << l;
 	#endif
 }
