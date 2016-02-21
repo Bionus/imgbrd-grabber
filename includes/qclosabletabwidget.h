b73635239a6291902f6801489193969711d5c756
Bionus <bio.nus@hotmail.fr>
2016-02-21 03:00:04 +0100

    Add tab closing by middle click (issue #470)

---
+++ includes/qclosabletabwidget.h
@@ -0,0 +1,15 @@
+#ifndef QCLOSABLETABBAR_H
+#define QCLOSABLETABBAR_H
+
+#include <QTabWidget>
+#include <QMouseEvent>
+
+
+class QClosableTabWidget : public QTabWidget
+{
+    public:
+        QClosableTabWidget(QWidget *parent);
+        bool eventFilter(QObject *o, QEvent *e);
+};
+
+#endif // QCLOSABLETABBAR_H
---
+++ source/qclosabletabwidget.cpp
@@ -0,0 +1,24 @@
+#include <QTabBar>
+#include "qclosabletabwidget.h"
+
+
+QClosableTabWidget::QClosableTabWidget(QWidget *parent)
+	: QTabWidget(parent)
+{
+	tabBar()->installEventFilter(this);
+}
+
+bool QClosableTabWidget::eventFilter(QObject *o, QEvent *e)
+{
+        if (o == tabBar() && e->type() == QEvent::MouseButtonPress)
+	{
+		QMouseEvent *mouseEvent = static_cast<QMouseEvent *>(e);
+                if (mouseEvent->button() == Qt::MiddleButton)
+                {
+                    removeTab(tabBar()->tabAt(mouseEvent->pos()));
+                    return true;
+                }
+	}
+
+	return QTabWidget::eventFilter(o, e);
+}
