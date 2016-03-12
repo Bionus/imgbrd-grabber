a4ddd888b7c66694b7a37fabc6d0d70f2d7e7131
Bionus <bio.nus@hotmail.fr>
2016-03-12 18:37:25 +0100

    Update unix build file to match new architecture

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ apt-get install -qq "qt5-qmake" "qtbase5-dev" "qtdeclarative5-dev" "qtscript5-de
 apt-get install "qt5-default" "qttools5-dev-tools"
 qmake "Grabber.pro"
 make
-mv "Grabber" "release/Grabber"
+mv "gui/Grabber" release
 touch "release/settings.ini"
 
 echo "Grabber has been compiled in the release directory. To run it, type './release/Grabber'"
