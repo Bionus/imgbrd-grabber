2dcb8451b267220b7b1504518037e028ae7c59cf
Bionus <bio.nus@hotmail.fr>
2016-08-21 20:17:40 +0200

    Add additional test for Filename::isEmpty coverage

--- tests/models/filename-test.cpp
+++ tests/models/filename-test.cpp
@@ -256,6 +256,10 @@ void FilenameTest::testIsValid()
     QCOMPARE(Filename("%website%/%id%.%ext%").isValid(), true);
     QCOMPARE(Filename("%artist%/%copyright%/%character%/%md5%.%ext%").isValid(), true);
     QCOMPARE(Filename("javascript:md5 + '.' + ext;").isValid(), true);
+
+    QString out;
+    Filename("%toto%.%ext%").isValid(&out);
+    QCOMPARE(out.isEmpty(), false);
 }
 
 void FilenameTest::testUseShorterCopyright()
