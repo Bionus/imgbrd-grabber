96c493e31b96fe20ac9f1f137c6fd2ac934d9b4e
Bionus <bio.nus@hotmail.fr>
2016-08-21 16:05:12 +0200

    Add tests for models/pool

--- src/models/pool.cpp
+++ src/models/pool.cpp
@@ -2,13 +2,14 @@
 
 
 
-Pool::Pool(int id, QString name, int current, int next, int previous) : m_name(name), m_id(id), m_current(current), m_next(next), m_previous(previous)
+Pool::Pool(int id, QString name, int current, int next, int previous)
+    : m_id(id), m_name(name), m_current(current), m_next(next), m_previous(previous)
 { }
 Pool::~Pool()
 { }
 
-QString	Pool::name()		{ return m_name;		}
 int		Pool::id()			{ return m_id;			}
+QString	Pool::name()		{ return m_name;		}
 int		Pool::current()		{ return m_current;		}
 int		Pool::next()		{ return m_next;		}
 int		Pool::previous()	{ return m_previous;	}
---
+++ tests/models/pool-test.cpp
@@ -0,0 +1,31 @@
+#include <QtTest>
+#include "pool-test.h"
+
+
+void PoolTest::testGetId()
+{
+    Pool pool(123, "Test pool", 1, 2, 3);
+    QCOMPARE(pool.id(), 123);
+}
+void PoolTest::testGetId()
+{
+    Pool pool(123, "Test pool", 1, 2, 3);
+    QCOMPARE(pool.name(), QString("Test pool"));
+}
+void PoolTest::testGetCurrent()
+{
+    Pool pool(123, "Test pool", 1, 2, 3);
+    QCOMPARE(pool.current(), 1);
+}
+void PoolTest::testGetNext()
+{
+    Pool pool(123, "Test pool", 1, 2, 3);
+    QCOMPARE(pool.next(), 2);
+}
+void PoolTest::testGetPrevious()
+{
+    Pool pool(123, "Test pool", 1, 2, 3);
+    QCOMPARE(pool.previous(), 3);
+}
+
+static PoolTest instance;
