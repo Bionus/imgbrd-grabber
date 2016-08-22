1423975d1c08c484ed285c2d5e29d4de9438df7a
Bionus <bio.nus@hotmail.fr>
2016-08-22 18:50:34 +0200

    Update Sankaku test to only use SFW resources

--- tests/integration/sankaku-test.cpp
+++ tests/integration/sankaku-test.cpp
@@ -24,7 +24,7 @@ void SankakuTest::myInit(QString source)
 	QList<Site*> sites;
     sites.append(new Site(m_settings, "release/sites/Sankaku", "idol.sankakucomplex.com"));
 
-    m_downloader = new Downloader(QStringList() << "animated",
+    m_downloader = new Downloader(QStringList() << "animated rating:safe",
 								  QStringList(),
 								  sites,
 								  1,
@@ -75,7 +75,7 @@ void SankakuTest::testHtml()
 
     // Check results
     md5s = md5s.mid(0, 3);
-    QStringList expected = QStringList() << "0c007c1cae6bb1327a87ea310b0ddf2a" << "3e58d450896af686e5022bbbb805a396" << "ca880454c31a5576ce701a3d58e165c4";
+    QStringList expected = QStringList() << "6e7901eea2a5a2d2b96244593ed190df" << "97b3355a7af0bfabc67f2678a4a837fd" << "d9f7f5089da4a677846d77da2c146088";
     QCOMPARE(images.count(), 20);
     QCOMPARE(md5s, expected);
 }
