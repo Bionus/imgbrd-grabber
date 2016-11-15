a3f5945d83b1754eabe0ddce939bf2ba40d892c5
Bionus <bio.nus@hotmail.fr>
2016-11-15 21:06:02 +0100

    English in detailsWindow

--- src/viewer/detailswindow.cpp
+++ src/viewer/detailswindow.cpp
@@ -40,7 +40,7 @@ void detailsWindow::setImage(QSharedPointer<Image> image)
 	if (image->id() != 0)				{ ui->labelId->setText(QString::number(image->id()));	}
 	if (!image->md5().isEmpty())		{ ui->labelMd5->setText(image->md5());					}
 	if (!image->rating().isEmpty())		{ ui->labelRating->setText(image->rating());			}
-	if (image->createdAt().isValid())	{ ui->labelDate->setText(image->createdAt().toString(tr("le dd/MM/yyyy à hh:mm")));		}
+	if (image->createdAt().isValid())	{ ui->labelDate->setText(image->createdAt().toString(tr("'the' MM/dd/yyyy 'at' hh:mm")));		}
 	if (!image->source().isEmpty())		{ ui->labelSource->setText("<a href=\""+image->source()+"\">"+image->source()+"</a>");	}
 	if (!image->pageUrl().isEmpty())	{ ui->labelPage->setText("<a href=\""+image->pageUrl().toString()+"\">"+image->pageUrl().toString()+"</a>");			}
 	if (!image->fileUrl().isEmpty())	{ ui->labelUrl->setText("<a href=\""+image->fileUrl().toString()+"\">"+image->fileUrl().toString()+"</a>");				}
@@ -56,8 +56,8 @@ void detailsWindow::setImage(QSharedPointer<Image> image)
 	if (!image->size().isEmpty())		{ ui->labelSize->setText(QString::number(image->width())+"x"+QString::number(image->height()));	}
 
 	ui->labelScore->setText(QString::number(image->score()));
-	ui->labelChildren->setText(image->hasChildren() ? tr("oui") : tr("non"));
-	ui->labelNotes->setText(image->hasNote() ? tr("oui") : tr("non"));
-	ui->labelComments->setText(image->hasComments() ? tr("oui") : tr("non"));
-	ui->labelParent->setText(image->parentId() != 0 ? tr("oui (#%1)").arg(image->parentId() != 0) : tr("non"));
+	ui->labelChildren->setText(image->hasChildren() ? tr("yes") : tr("no"));
+	ui->labelNotes->setText(image->hasNote() ? tr("yes") : tr("no"));
+	ui->labelComments->setText(image->hasComments() ? tr("yes") : tr("no"));
+	ui->labelParent->setText(image->parentId() != 0 ? tr("yes (#%1)").arg(image->parentId() != 0) : tr("no"));
 }
