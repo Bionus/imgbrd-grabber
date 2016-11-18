10144e552a4a4a4ad903986d74e34712031feeba
Bionus <bio.nus@hotmail.fr>
2016-11-18 23:49:18 +0100

    Improve performance of image reader thread

--- src/imagethread.cpp
+++ src/imagethread.cpp
@@ -1,12 +1,13 @@
 #include "imagethread.h"
 
-ImageThread::ImageThread(QByteArray data, QObject* parent) : QThread(parent), m_data(data)
-{
-}
+
+ImageThread::ImageThread(const QByteArray &data, QObject* parent)
+	: QThread(parent), m_data(data)
+{ }
 
 void ImageThread::run()
 {
 	QImage image;
 	if (image.loadFromData(m_data))
-	{ emit finished(image, m_data.size()); }
+		emit finished(image, m_data.size());
 }
--- src/imagethread.h
+++ src/imagethread.h
@@ -1,9 +1,8 @@
-#ifndef IMAGETHREAD_H
-#define IMAGETHREAD_H
+#ifndef IMAGE_THREAD_H
+#define IMAGE_THREAD_H
 
 #include <QThread>
 #include <QPixmap>
-
 
 
 class ImageThread : public QThread
@@ -11,7 +10,7 @@ class ImageThread : public QThread
 	Q_OBJECT
 
 	public:
-		ImageThread(QByteArray data, QObject* parent = 0);
+		ImageThread(const QByteArray &data, QObject* parent = Q_NULLPTR);
 
 	protected:
 		void run();
@@ -21,7 +20,7 @@ class ImageThread : public QThread
 		void finished(QImage, int);
 
 	private:
-		QByteArray m_data;
+		const QByteArray &m_data;
 };
 
-#endif // IMAGETHREAD_H
+#endif // IMAGE_THREAD_H
