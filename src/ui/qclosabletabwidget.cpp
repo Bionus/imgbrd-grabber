dd41ddd2917f64967f1a119c6e4d0a5674f6e2ed
Bionus <bio.nus@hotmail.fr>
2017-01-03 00:19:49 +0100

    Disable middle click close for unclosable tabs

--- src/ui/qclosabletabwidget.cpp
+++ src/ui/qclosabletabwidget.cpp
@@ -16,9 +16,15 @@ bool QClosableTabWidget::eventFilter(QObject *o, QEvent *e)
 		if (mouseEvent->button() == Qt::MiddleButton)
 		{
 			int index = tabBar()->tabAt(mouseEvent->pos());
-			widget(index)->deleteLater();
-			removeTab(index);
-			return true;
+			QWidget *w = widget(index);
+
+			// Unclosable tabs have a maximum width of 16777214 (default: 16777215)
+			if (w->maximumWidth() != 16777214)
+			{
+				w->deleteLater();
+				removeTab(index);
+				return true;
+			}
 		}
 	}
 
