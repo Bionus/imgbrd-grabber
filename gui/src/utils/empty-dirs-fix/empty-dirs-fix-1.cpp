116daed086ad2a8a01403a77443a337fbcf507d7
Bionus <bio.nus@hotmail.fr>
2017-02-25 02:03:16 +0100

    Improve popup for empty folders fixer when there are no results

--- gui/src/utils/empty-dirs-fix/empty-dirs-fix-1.cpp
+++ gui/src/utils/empty-dirs-fix/empty-dirs-fix-1.cpp
@@ -1,4 +1,5 @@
 #include <QSettings>
+#include <QMessageBox>
 #include "empty-dirs-fix-1.h"
 #include "empty-dirs-fix-2.h"
 #include "ui_empty-dirs-fix-1.h"
@@ -18,9 +19,47 @@ EmptyDirsFix1::~EmptyDirsFix1()
 	delete ui;
 }
 
+
 void EmptyDirsFix1::next()
 {
-	EmptyDirsFix2 *edf2 = new EmptyDirsFix2(ui->lineFolder->text());
+	QStringList dirs = mkList(QDir(ui->lineFolder->text()));
+
+	// We don't continue if there were no folders found
+	if (dirs.isEmpty())
+	{
+		QMessageBox::information(this, tr("Empty folders fixer"), tr("No empty folder found."));
+		close();
+		return;
+	}
+
+	EmptyDirsFix2 *edf2 = new EmptyDirsFix2(dirs);
 	close();
 	edf2->show();
+}
+
+QStringList EmptyDirsFix1::mkList(QDir dir)
+{
+	QStringList ret;
+	QStringList dirs = dir.entryList(QDir::Dirs | QDir::NoDotAndDotDot);
+	for (int i = 0; i < dirs.size(); i++)
+	{
+		if (isEmpty(QDir(dir.path()+"/"+dirs.at(i))))
+		{ ret.append(dir.path()+"/"+dirs.at(i)); }
+		else
+		{ mkList(QDir(dir.path()+"/"+dirs.at(i))); }
+	}
+	return ret;
+}
+
+bool EmptyDirsFix1::isEmpty(QDir dir)
+{
+	QStringList files = dir.entryList(QDir::Files);
+	if (!files.isEmpty())
+	{ return false; }
+
+	QStringList dirs = dir.entryList(QDir::Dirs | QDir::NoDotAndDotDot);
+	bool empty = true;
+	for (int i = 0; i < dirs.size(); i++)
+	{ empty = empty && isEmpty(QDir(dir.path()+"/"+dirs.at(i))); }
+	return empty;
 }
