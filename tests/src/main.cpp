23a838f8337158df1c4f22aa1f260e330c8dd0ac
Bionus <bio.nus@hotmail.fr>
2017-03-25 12:23:34 +0100

    Allow to only run some test suites from the test runner

--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -12,6 +12,10 @@ int main(int argc, char *argv[])
 		QGuiApplication a(argc, argv);
 	#endif
 
+	QStringList testSuites;
+	for (int i = 1; i < argc; ++i)
+		testSuites.append(argv[i]);
+
 	QMap<QString,int> results;
 	int failed = 0;
 
@@ -19,6 +23,9 @@ int main(int argc, char *argv[])
 
 	for (QObject *suite : TestSuite::suites)
 	{
+		if (!testSuites.isEmpty() && !testSuites.contains(suite->metaObject()->className()))
+			continue;
+
 		int result = QTest::qExec(suite);
 		results.insert(suite->metaObject()->className(), result);
 		if (result != 0)
