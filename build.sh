a1171abc7f8e120d316004ce2be4702e0064a978
Bionus <bio.nus@hotmail.fr>
2017-06-11 03:49:09 +0200

    Update build files

--- build.sh
+++ build.sh
@@ -3,12 +3,15 @@
 add-apt-repository --yes "ppa:ubuntu-sdk-team/ppa"
 apt-get update -qq
 apt-get install -qq "qt5-qmake" "qtbase5-dev" "qtdeclarative5-dev" "qtscript5-dev" "qtmultimedia5-dev" "libpulse-dev" "qt5-default" "qttools5-dev-tools"
-apt-get install -qq "g++"
+apt-get install -qq "g++" "cmake"
 
-QT_SELECT=5 qmake "Grabber.pro"
-make
+mkdir build
+cd build
+cmake ..
+make -j8
+cd ..
 
-mv "gui/build/release/Grabber" release
+mv "build/gui/gui" "release/Grabber"
 touch "release/settings.ini"
 
 echo "Grabber has been compiled in the release directory. To run it, type './release/Grabber'"
