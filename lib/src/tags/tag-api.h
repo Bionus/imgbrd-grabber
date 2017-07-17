78a0ee65a8ffb1f8baa10a593c361a75428ea28c
Bionus <bio.nus@hotmail.fr>
2017-07-17 22:20:18 +0200

    Add new TagApi class to load tag information

---
+++ lib/src/tags/tag-api.h
@@ -0,0 +1,48 @@
+#ifndef TAG_API_H
+#define TAG_API_H
+
+#include <QNetworkReply>
+#include "tag.h"
+
+
+class Profile;
+class Api;
+class Site;
+
+class TagApi : public QObject
+{
+	Q_OBJECT
+
+	public:
+		enum LoadResult
+		{
+			Ok,
+			Error
+		};
+
+		explicit TagApi(Profile *profile, Site *site, Api *api, int page = 1, int limit = 1000, QObject *parent = Q_NULLPTR);
+		~TagApi();
+		void load(bool rateLimit = false);
+		QList<Tag> tags() const;
+
+	public slots:
+		void abort();
+
+	protected slots:
+		void parse();
+
+	signals:
+		void finishedLoading(TagApi *api, LoadResult status);
+
+	private:
+		Profile *m_profile;
+		Site *m_site;
+		Api *m_api;
+		int m_page;
+		int m_limit;
+		QUrl m_url;
+		QNetworkReply *m_reply;
+		QList<Tag> m_tags;
+};
+
+#endif // TAG_API_H
