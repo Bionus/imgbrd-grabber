04464a20b1041286f141954c11f709aea8273150
Bionus <bio.nus@hotmail.fr>
2017-08-06 21:34:32 +0200

    Remove requirement on ubuntu-sdk-team/ppa (fix #1004)

--- build.sh
+++ build.sh
@@ -6,8 +6,6 @@ then
   sudo pacman -Sy
   sudo pacman -S "qt" "gcc" "cmake" "libpulse"
 else
-  sudo add-apt-repository --yes "ppa:ubuntu-sdk-team/ppa"
-  sudo apt-get update -qq
   sudo apt-get install -qq "qt5-qmake" "qtbase5-dev" "qtdeclarative5-dev" "qtscript5-dev" "qtmultimedia5-dev" "libpulse-dev" "qt5-default" "qttools5-dev-tools"
   sudo apt-get install -qq "g++" "cmake"
 fi
