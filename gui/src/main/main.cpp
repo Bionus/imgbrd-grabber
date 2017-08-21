5269c332f0210e11b044b7f6d8da3675ca8dcaed
Bionus <bio.nus@hotmail.fr>
2017-08-21 21:33:38 +0200

    Check each moveable setting separately (issue #1018)

--- gui/src/main/main.cpp
+++ gui/src/main/main.cpp
@@ -71,16 +71,13 @@ int main(int argc, char *argv[])
 	#endif
 
 	// Copy settings files to writable directory
-	if (!QDir(savePath("sites/", true, true)).exists())
+	QStringList toCopy = QStringList() << "sites/" << "themes/" << "webservices/";
+	for (QString tgt : toCopy)
 	{
-		QStringList toCopy = QStringList() << "sites/" << "themes/" << "webservices/";
-		for (QString tgt : toCopy)
-		{
-			QString from = savePath(tgt, true, false);
-			QString to = savePath(tgt, true, true);
-			if (!QDir(to).exists() && QDir(from).exists())
-				copyRecursively(from, to);
-		}
+		QString from = savePath(tgt, true, false);
+		QString to = savePath(tgt, true, true);
+		if (!QDir(to).exists() && QDir(from).exists())
+			copyRecursively(from, to);
 	}
 
 	QCommandLineParser parser;
