fff1323789c5144ce63c01438b3ce10c9a28a001
Bionus <bio.nus@hotmail.fr>
2017-08-31 10:25:22 +0200

    Show progress for tag loader (issue #1046)

--- gui/src/utils/tag-loader/tag-loader.cpp
+++ gui/src/utils/tag-loader/tag-loader.cpp
@@ -15,7 +15,7 @@ TagLoader::TagLoader(Profile *profile, QMap<QString, Site *> sites, QWidget *par
 	ui->setupUi(this);
 
 	ui->comboSource->addItems(m_sites.keys());
-	ui->progressBar->hide();
+	ui->widgetProgress->hide();
 
 	resize(size().width(), 0);
 }
@@ -34,23 +34,23 @@ void TagLoader::cancel()
 
 void TagLoader::start()
 {
-    // Get site and API
-    Site *site = m_sites.value(ui->comboSource->currentText());
-    Api *api = Q_NULLPTR;
-    for (Api *a : site->getApis())
-    {
-        if (a->contains("Urls/TagApi"))
-        {
-            api = a;
-            break;
-        }
-    }
-    if (api == Q_NULLPTR)
-    {
-        error(this, tr("No API supporting tag fetching found"));
-        return;
-    }
-    site->tagDatabase()->load();
+	// Get site and API
+	Site *site = m_sites.value(ui->comboSource->currentText());
+	Api *api = Q_NULLPTR;
+	for (Api *a : site->getApis())
+	{
+		if (a->contains("Urls/TagApi"))
+		{
+			api = a;
+			break;
+		}
+	}
+	if (api == Q_NULLPTR)
+	{
+		error(this, tr("No API supporting tag fetching found"));
+		return;
+	}
+	site->tagDatabase()->load();
 
 	ui->buttonStart->setEnabled(false);
 
@@ -58,7 +58,8 @@ void TagLoader::start()
 	ui->progressBar->setValue(0);
 	ui->progressBar->setMinimum(0);
 	ui->progressBar->setMaximum(0);
-	ui->progressBar->show();
+	ui->labelProgress->setText("0");
+	ui->widgetProgress->show();
 
 	// Load all tags
 	QList<Tag> allTags;
@@ -78,6 +79,7 @@ void TagLoader::start()
 		tagApi->deleteLater();
 
 		ui->progressBar->setValue(page);
+		ui->labelProgress->setText(QString::number(page));
 		page++;
 	}
 
@@ -87,7 +89,7 @@ void TagLoader::start()
 
 	// Hide progress bar
 	ui->buttonStart->setEnabled(true);
-	ui->progressBar->hide();
+	ui->widgetProgress->hide();
 	resize(size().width(), 0);
 
 	QMessageBox::information(this, tr("Finished"), tr("%n tag(s) loaded", "", allTags.count()));
