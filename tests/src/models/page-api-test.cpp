c73ef59fce14f1dc61df522cc55a3c9f017e7fa1
Bionus <bio.nus@hotmail.fr>
2017-12-30 12:53:57 +0100

    Fix PageApi tests

--- tests/src/models/page-api-test.cpp
+++ tests/src/models/page-api-test.cpp
@@ -5,14 +5,29 @@
 
 void PageApiTest::init()
 {
-	m_sites.append(new Site("danbooru.donmai.us", new Source(&profile, "release/sites/Danbooru (2.0)")));
-	m_site = new Site("gelbooru.com", new Source(&profile, "release/sites/Gelbooru (0.2)"));
+	setupSource("Danbooru (2.0)");
+	setupSite("Danbooru (2.0)", "danbooru.donmai.us");
+
+	setupSource("Gelbooru (0.2)");
+	setupSite("Gelbooru (0.2)", "gelbooru.com");
+
+	QString path = "tests/resources/sites/Danbooru (2.0)/danbooru.donmai.us/defaults.ini";
+	QSettings settings(path, QSettings::IniFormat);
+	settings.setValue("auth/pseudo", "user");
+	settings.setValue("auth/password", "pass");
+	settings.sync();
+
+	m_sites.append(new Site("danbooru.donmai.us", new Source(&profile, "tests/resources/sites/Danbooru (2.0)")));
+	m_site = new Site("gelbooru.com", new Source(&profile, "tests/resources/sites/Gelbooru (0.2)"));
+
 }
 
 void PageApiTest::cleanup()
 {
 	m_sites.first()->deleteLater();
 	m_site->deleteLater();
+
+	QFile::remove("tests/resources/sites/Danbooru (2.0)/danbooru.donmai.us/defaults.ini");
 }
 
 
@@ -30,21 +45,12 @@ void PageApiTest::testParseUrlLogin()
 {
 	Site *site = m_sites.first();
 
-	QString path = "tests/resources/sites/Danbooru (2.0)/danbooru.donmai.us/defaults.ini";
-	QSettings settings(path, QSettings::IniFormat);
-	settings.setValue("auth/pseudo", "user");
-	settings.setValue("auth/password", "pass");
-	settings.sync();
-	site->loadConfig();
-
 	QStringList tags = QStringList() << "test" << "tag";
 	Page page(&profile, site, m_sites, tags);
 	PageApi pageApi(&page, &profile, site, site->getApis().first(), tags);
 
 	QCOMPARE(pageApi.parseUrl("/posts.xml?{login}limit={limit}&page={page}{altpage}&tags={tags}").toString(),
 			 QString("https://danbooru.donmai.us/posts.xml?login=user&password_hash=pass&limit=25&page=1&tags=test tag"));
-
-	QFile::remove(path);
 }
 
 void PageApiTest::testParseUrlAltPage()
