216194ff87f7c6d3199ab363582e1813bb233ece
Bionus <bio.nus@hotmail.fr>
2018-10-02 17:12:23 +0200

    Use new auth classes in SourceSettingsWindow

--- gui/src/sources/sources-settings-window.cpp
+++ gui/src/sources/sources-settings-window.cpp
@@ -5,6 +5,7 @@
 #include <QNetworkCookie>
 #include <QSettings>
 #include <ui_sources-settings-window.h>
+#include "auth/field-auth.h"
 #include "auth/auth-field.h"
 #include "auth/auth-hash-field.h"
 #include "functions.h"
@@ -29,6 +30,20 @@ SourcesSettingsWindow::SourcesSettingsWindow(Profile *profile, Site *site, QWidg
 {
 	setAttribute(Qt::WA_DeleteOnClose);
 	ui->setupUi(this);
+
+	auto auth = dynamic_cast<FieldAuth*>(site->getAuth());
+	if (auth != nullptr)
+	{
+		for (AuthField *field : auth->fields())
+		{
+			if (field->type() == AuthField::Hash)
+			{
+				auto hashField = dynamic_cast<AuthHashField*>(field);
+				m_salt = hashField->salt();
+				break;
+			}
+		}
+	}
 
 	// Refferers
 	ui->lineSiteName->setText(site->setting("name", m_site->url()).toString());
@@ -94,19 +109,7 @@ SourcesSettingsWindow::SourcesSettingsWindow(Profile *profile, Site *site, QWidg
 	ui->spinLoginMaxPage->setValue(site->setting("login/maxPage", 0).toInt());
 
 	// Hide hash if unncessary
-	bool hasHash = false;
-	if (site->getAuth() != nullptr)
-	{
-		for (AuthField *field : site->getAuth()->fields())
-		{
-			if (field->type() == AuthField::Hash)
-			{
-				hasHash = true;
-				break;
-			}
-		}
-	}
-	if (!hasHash)
+	if (m_salt.isEmpty())
 	{ ui->buttonAuthHash->hide(); }
 	else
 	{ ui->lineAuthPassword->setEchoMode(QLineEdit::Normal); }
@@ -159,22 +162,11 @@ void SourcesSettingsWindow::addHeader()
 
 void SourcesSettingsWindow::on_buttonAuthHash_clicked()
 {
-	QString salt;
-	for (AuthField *field : m_site->getAuth()->fields())
-	{
-		if (field->type() == AuthField::Hash)
-		{
-			auto hashField = dynamic_cast<AuthHashField*>(field);
-			salt = hashField->salt();
-			break;
-		}
-	}
-
-	QString password = QInputDialog::getText(this, tr("Hash a password"), tr("Please enter your password below.<br/>It will then be hashed using the format \"%1\".").arg(salt));
+	QString password = QInputDialog::getText(this, tr("Hash a password"), tr("Please enter your password below.<br/>It will then be hashed using the format \"%1\".").arg(m_salt));
 	if (!password.isEmpty())
 	{
-		salt.replace("%password%", password);
-		ui->lineAuthPassword->setText(QCryptographicHash::hash(salt.toUtf8(), QCryptographicHash::Sha1).toHex());
+		QString val = QString(m_salt).replace("%password%", password);
+		ui->lineAuthPassword->setText(QCryptographicHash::hash(val.toUtf8(), QCryptographicHash::Sha1).toHex());
 	}
 }
 
