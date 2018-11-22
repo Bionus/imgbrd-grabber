31f5d4d7b6d91a04141fd4f7a7fd5ffdfaad2ab3
Bionus <bio.nus@hotmail.fr>
2018-11-22 12:15:08 +0100

    Switch to cotire for pre-compiled headers

--- lib/src/pch.h
+++
@@ -1,8 +0,0 @@
-#include <QtGlobal>
-#include <QObject>
-
-#include <QDir>
-#include <QList>
-#include <QMap>
-#include <QString>
-#include <QStringList>
--- tests/src/pch.h
+++
@@ -1,9 +0,0 @@
-#include <QtGlobal>
-#include <QtTest>
-
-#include <QList>
-#include <QMap>
-#include <QString>
-#include <QStringList>
-
-#include "test-suite.h"
