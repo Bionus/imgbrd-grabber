cbb4186599c8ffb61b66d992e8ff40850faf6c97
Jack Vasti <bio.nus@hotmail.fr>
2019-05-15 11:59:44 +0200

    New translations YourLanguage.ts (Korean)

--- languages/Korean.ts
+++ languages/Korean.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="ko_KR" sourcelanguage="en_US">
+<TS version="2.1" language="ko" sourcelanguage="en">
   <context>
     <name>AboutWindow</name>
     <message>
