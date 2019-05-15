b23ef50df20b4a42a82d9864f1a438d1d35805f8
Jack Vasti <bio.nus@hotmail.fr>
2019-05-15 11:59:39 +0200

    New translations YourLanguage.ts (Japanese)

--- languages/Japanese.ts
+++ languages/Japanese.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="ja_JP" sourcelanguage="en_US">
+<TS version="2.1" language="ja" sourcelanguage="en">
   <context>
     <name>AboutWindow</name>
     <message>
