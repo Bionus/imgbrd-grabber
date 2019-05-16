f0e076044b2c02ce239d8f988cffa22e4ddf475e
Jack Vasti <bio.nus@hotmail.fr>
2019-05-17 00:01:46 +0200

    New translations YourLanguage.ts (Spanish)

--- CrashReporter/languages/Spanish.ts
+++ CrashReporter/languages/Spanish.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="es_MX" sourcelanguage="en_US">
+<TS version="2.1" language="es-ES" sourcelanguage="en">
   <context>
     <name>CrashReporterWindow</name>
     <message>
