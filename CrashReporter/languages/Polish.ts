550227009b5848610cc29e2807cf9e79e227cbf5
Jack Vasti <bio.nus@hotmail.fr>
2019-06-10 01:05:43 +0200

    New translations YourLanguage.ts (Polish)

--- CrashReporter/languages/Polish.ts
+++ CrashReporter/languages/Polish.ts
@@ -6,52 +6,52 @@
     <message>
       <location filename="../crash-reporter-window.ui" line="14"/>
       <source>Crash Reporter</source>
-      <translation type="unfinished"/>
+      <translation>Raport błędów</translation>
     </message>
     <message>
       <location filename="../crash-reporter-window.ui" line="28"/>
       <source>Sorry</source>
-      <translation type="unfinished"/>
+      <translation>Przepraszamy</translation>
     </message>
     <message>
       <location filename="../crash-reporter-window.ui" line="44"/>
       <source>Grabber encountered a problem and crashed. The program will try to restore your tabs and other settings when it restarts.</source>
-      <translation type="unfinished"/>
+      <translation>Grabber napotkał problem i uległ awarii. Program spróbuje przywrócić karty i inne ustawienia po ponownym uruchomieniu.</translation>
     </message>
     <message>
       <location filename="../crash-reporter-window.ui" line="60"/>
       <source>To help us fix this crash, you can send us a bug report.</source>
-      <translation type="unfinished"/>
+      <translation>Aby nam pomóc w naprawieniu problemu, możesz wysłać nam raport o błędzie.</translation>
     </message>
     <message>
       <location filename="../crash-reporter-window.ui" line="70"/>
       <source>Send a bug report</source>
-      <translation type="unfinished"/>
+      <translation>Wyślij raport o błędzie</translation>
     </message>
     <message>
       <location filename="../crash-reporter-window.ui" line="95"/>
       <source>Log</source>
-      <translation type="unfinished"/>
+      <translation>Logi</translation>
     </message>
     <message>
       <location filename="../crash-reporter-window.ui" line="102"/>
       <source>Settings</source>
-      <translation type="unfinished"/>
+      <translation>Ustawienia</translation>
     </message>
     <message>
       <location filename="../crash-reporter-window.ui" line="109"/>
       <source>Dump</source>
-      <translation type="unfinished"/>
+      <translation>Zrzut</translation>
     </message>
     <message>
       <location filename="../crash-reporter-window.ui" line="155"/>
       <source>Restart</source>
-      <translation type="unfinished"/>
+      <translation>Uruchom ponownie</translation>
     </message>
     <message>
       <location filename="../crash-reporter-window.ui" line="162"/>
       <source>Quit</source>
-      <translation type="unfinished"/>
+      <translation>Wyjdź</translation>
     </message>
   </context>
 </TS>
