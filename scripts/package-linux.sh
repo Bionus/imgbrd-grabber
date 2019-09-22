505c7cb13d7ad1b493c99a6af48519d0229100a6
Bionus <bio.nus@hotmail.fr>
2019-09-22 15:29:03 +0200

    Package Linux releases as tar.gz instead of zip

--- scripts/package-linux.sh
+++ scripts/package-linux.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Package everything into a "Grabber.zip" file at the root of the git repository
+# Package everything into a "Grabber.tar.gz" file at the root of the git repository
 
 # Copy all required files to the release directory
 cp "build/gui/Grabber" "release/Grabber"
@@ -9,5 +9,5 @@ rm "release/CDR.exe"
 
 # Zip the whole directory
 pushd release
-    zip -r "../Grabber.zip" . -x "sites/node_modules/*"
+    tar -czvf "../Grabber.tar.gz" --exclude="./sites/node_modules" .
 popd
