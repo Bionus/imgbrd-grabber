41f164b22639115fcc3c6f5f4ebe32a4689ea4cc
Bionus <bio.nus@hotmail.fr>
2020-07-06 11:46:48 +0200

    Add TS test helper to build a global Grabber instance

--- src/sites/test-utils.ts
+++ src/sites/test-utils.ts
@@ -1,7 +1,59 @@
+import { Parser } from "xml2js";
+
 export function search(api: IApi, query: string, page: number = 1) {
     return api.search.url(
         { search: query, page },
         { limit: 10, loggedIn: false, baseUrl: "/" },
         undefined
     );
+}
+
+function GrabberRegexMatches(regex: string, txt: string): any[] {
+    const rx = new RegExp(regex, "g");
+    const ret = [];
+    let val: any;
+    while ((val = rx.exec(txt)) !== null) {
+        ret.push({ ...val, ...val.groups });
+    }
+    return ret;
+}
+
+function syncXml2jsParseString(parser: any, xml: string): any {
+    var error = null;
+    var json = null;
+    parser.parseString(xml, (innerError: any, innerJson: any) => {
+        error = innerError;
+        json = innerJson;
+    });
+    if (error) {
+        throw error;
+    }
+    if (!error && !json) {
+        throw new Error('The callback was suddenly async or something.');
+    }
+    return json;
+}
+
+function GrabberParseXML(txt: string) {
+    const parser = new Parser({
+        attrkey: "@attributes",
+        charkey: "#text",
+        explicitCharkey: true,
+        explicitArray: false,
+    });
+    try {
+        return syncXml2jsParseString(parser, txt);
+    } catch (e) {
+        return null;
+    }
+}
+
+export function makeGrabber() {
+    (global as any).Grabber = {
+        htmlDecode(txt: string) { return txt },
+        regexMatches: GrabberRegexMatches,
+        parseXML: GrabberParseXML,
+    };
+
+    require("./helper");
 }
