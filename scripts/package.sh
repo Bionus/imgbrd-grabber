e922b5d7425ca51f917e24f525579b3c17e14120
Bionus <bio.nus@hotmail.fr>
2020-07-06 12:20:27 +0200

    Use rsync when packaging to avoid copying node_modules

--- scripts/package.sh
+++ scripts/package.sh
@@ -8,8 +8,7 @@ mkdir -p $APP_DIR
 
 # Copy all required files to the release directory
 cp "build/gui/Grabber" $APP_DIR
-cp -r src/sites/ $APP_DIR
+rsync -ar --exclude="node_modules" --exclude="**/*.ts" --exclude="**/resources" src/sites $APP_DIR
 cp -r src/dist/common/* $APP_DIR
 cp build/languages/*.qm "$APP_DIR/languages/"
 touch "$APP_DIR/settings.ini"
-rm -rf "$APP_DIR/sites/node_modules/"
