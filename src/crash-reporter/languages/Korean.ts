04f780047f669b2869cb174b67cd94f7f3e0853c
Jack Vasti <bio.nus@hotmail.fr>
2020-07-29 19:21:52 +0200

    New translations YourLanguage.ts (Korean)

--- src/crash-reporter/languages/Korean.ts
+++ src/crash-reporter/languages/Korean.ts
@@ -6,52 +6,52 @@
     <message>
       <location filename="../crash-reporter-window.ui" line="14"/>
       <source>Crash Reporter</source>
-      <translation>오류 보고</translation>
+      <translation type="unfinished"/>
     </message>
     <message>
       <location filename="../crash-reporter-window.ui" line="28"/>
       <source>Sorry</source>
-      <translation>이런!</translation>
+      <translation type="unfinished"/>
     </message>
     <message>
       <location filename="../crash-reporter-window.ui" line="44"/>
       <source>Grabber encountered a problem and crashed. The program will try to restore your tabs and other settings when it restarts.</source>
-      <translation>예상치 못한 오류가 발생하여 Grabber가 종료되었습니다. 열려있던 탭과 설정들은 재시작 시 복구될 것입니다.</translation>
+      <translation type="unfinished"/>
     </message>
     <message>
       <location filename="../crash-reporter-window.ui" line="60"/>
       <source>To help us fix this crash, you can send us a bug report.</source>
-      <translation>이 오류를 해결하기 위해 버그 리포트를 전송할 수 있습니다.</translation>
+      <translation type="unfinished"/>
     </message>
     <message>
       <location filename="../crash-reporter-window.ui" line="70"/>
       <source>Send a bug report</source>
-      <translation>버그 리포트 전송</translation>
+      <translation type="unfinished"/>
     </message>
     <message>
       <location filename="../crash-reporter-window.ui" line="95"/>
       <source>Log</source>
-      <translation>로그</translation>
+      <translation type="unfinished"/>
     </message>
     <message>
       <location filename="../crash-reporter-window.ui" line="102"/>
       <source>Settings</source>
-      <translation>설정</translation>
+      <translation type="unfinished"/>
     </message>
     <message>
       <location filename="../crash-reporter-window.ui" line="109"/>
       <source>Dump</source>
-      <translation>덤프</translation>
+      <translation type="unfinished"/>
     </message>
     <message>
       <location filename="../crash-reporter-window.ui" line="155"/>
       <source>Restart</source>
-      <translation>재시작</translation>
+      <translation type="unfinished"/>
     </message>
     <message>
       <location filename="../crash-reporter-window.ui" line="162"/>
       <source>Quit</source>
-      <translation>종료</translation>
+      <translation type="unfinished"/>
     </message>
   </context>
 </TS>
