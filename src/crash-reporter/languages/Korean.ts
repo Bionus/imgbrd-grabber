99824c9cf01823fa1a91306d64cec6b1b13d1034
Jack Vasti <bio.nus@hotmail.fr>
2020-07-29 19:07:39 +0200

    New translations YourLanguage.ts (Korean)

---
+++ src/crash-reporter/languages/Korean.ts
@@ -0,0 +1,57 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ko" sourcelanguage="en">
+  <context>
+    <name>CrashReporterWindow</name>
+    <message>
+      <location filename="../crash-reporter-window.ui" line="14"/>
+      <source>Crash Reporter</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <location filename="../crash-reporter-window.ui" line="28"/>
+      <source>Sorry</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <location filename="../crash-reporter-window.ui" line="44"/>
+      <source>Grabber encountered a problem and crashed. The program will try to restore your tabs and other settings when it restarts.</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <location filename="../crash-reporter-window.ui" line="60"/>
+      <source>To help us fix this crash, you can send us a bug report.</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <location filename="../crash-reporter-window.ui" line="70"/>
+      <source>Send a bug report</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <location filename="../crash-reporter-window.ui" line="95"/>
+      <source>Log</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <location filename="../crash-reporter-window.ui" line="102"/>
+      <source>Settings</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <location filename="../crash-reporter-window.ui" line="109"/>
+      <source>Dump</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <location filename="../crash-reporter-window.ui" line="155"/>
+      <source>Restart</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <location filename="../crash-reporter-window.ui" line="162"/>
+      <source>Quit</source>
+      <translation type="unfinished"/>
+    </message>
+  </context>
+</TS>
