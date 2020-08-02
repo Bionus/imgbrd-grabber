7e7df5ef0eee11ed2df486e3a3a51c333d99b8f5
Bionus <bio.nus@hotmail.fr>
2020-08-02 14:46:22 +0200

    Add github action to check coding style

---
+++ scripts/uncrustify.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+# "src/gui/src/" "src/tests/src/"
+find "src/lib/src/" "src/cli/src/" "src/e2e/src/" -name '*.cpp' -o -name '*.h' \
+| grep -v "vendor" \
+| grep -v "crashhandler" \
+| grep -v "backports" \
+| uncrustify -c "uncrustify.cfg" --replace --no-backup -F -
