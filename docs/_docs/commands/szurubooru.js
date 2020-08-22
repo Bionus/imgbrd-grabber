57e933ff795256a54596e9f513499607f48e11df
Bionus <bio.nus@hotmail.fr>
2020-08-22 14:43:33 +0200

    Use Axios in szurubooru commands script

--- docs/_docs/commands/szurubooru.js
+++ docs/_docs/commands/szurubooru.js
@@ -1,59 +1,50 @@
 // Usage:
 // node szurubooru.js "username" "token" "tag1 tag2" "safe" "http://source" "path/to/file.jpg"
 
-const http = require("http");
+const axios = require("axios");
 const fs = require("fs");
 const FormData = require("form-data");
 
-// Szurubooru doesn't use the same ratings as most boorus so we need to map them
-const ratingsMap = {
-    "safe": "safe",
-    "questionable": "sketchy",
-    "explicit": "unsafe",
-};
+(async () => {
+    // Szurubooru doesn't use the same ratings as most boorus so we need to map them
+    const ratingsMap = {
+        "safe": "safe",
+        "questionable": "sketchy",
+        "explicit": "unsafe",
+    };
 
-// Get parameters
-const argv = process.argv.slice(2);
-const username = argv.shift();
-const token = argv.shift();
+    // Get parameters
+    const argv = process.argv.slice(2);
+    const username = argv.shift();
+    const token = argv.shift();
 
-// Actual data to send to the server
-const data = {
-    "tags": argv[0].split(" "),
-    "safety": ratingsMap[argv[1]],
-    "source": argv[2] || undefined,
-};
-const headers = {
-    "Authorization": "Token " + Buffer.from(username + ":" + token).toString("base64"),
-    "Accept": "application/json",
-};
+    // Axios settings
+    axios.defaults.baseURL = "http://localhost:8080/api";
+    axios.defaults.headers.common["Authorization"] = "Token " + Buffer.from(username + ":" + token).toString("base64");
+    axios.defaults.headers.common["Accept"] = "application/json";
 
-// Create a multipart form data request
-const form = new FormData();
-form.append("content", fs.createReadStream(argv[3]), "test.tmp");
-form.append("metadata", JSON.stringify(data));
+    // Actual data to send to the server
+    const data = {
+        "tags": argv[0].split(" "),
+        "safety": ratingsMap[argv[1]],
+        "source": argv[2] || undefined,
+    };
 
-// Request settings
-// See https://github.com/rr-/szurubooru/blob/master/doc/API.md#creating-post
-const opts = {
-    hostname: "localhost",
-    port: 8080,
-    path: "/api/posts/",
-    method: "POST",
-    headers: { ...headers, ...form.getHeaders() },
-};
+    // Create a multipart form data request
+    const form = new FormData();
+    form.append("content", fs.createReadStream(argv[3]), "test.tmp");
+    form.append("metadata", JSON.stringify(data));
 
-// Send the request
-const req = http.request(opts, (res) => {
-    if (res.statusCode < 200 || res.statusCode >= 300) {
-        console.log("Error");
-        res.on('data', (d) => {
-            process.stdout.write(d);
-        });
-    } else {
-        console.log("OK");
+    // Create image post
+    try {
+        const config = {
+            headers: form.getHeaders(),
+            maxContentLength: 999999999999,
+            maxBodyLength: 999999999999,
+        }
+        await axios.post("/posts/", form, config);
+    } catch (e) {
+        console.error("Error creating post: " + e.message);
+        console.error(e.response.data);
     }
-}).on('error', function(e) {
-    console.error(e);
-});
-form.pipe(req);
+})();
