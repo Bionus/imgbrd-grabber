f2bb2cd1d95695a0c1342e2c6909b457b5732d63
Bionus <bio.nus@hotmail.fr>
2020-10-04 12:27:22 +0200

    Fix build by commenting out URL downloader manager

--- src/lib/src/models/profile.cpp
+++ src/lib/src/models/profile.cpp
@@ -162,7 +162,7 @@ Profile::Profile(QString path)
 	m_downloadQueryManager = new DownloadQueryManager(m_path + "/restore.igl", this);
 
 	// URL downloaders
-	m_urlDownloaderManager = new UrlDownloaderManager(m_path + "/sites", this);
+	// m_urlDownloaderManager = new UrlDownloaderManager(m_path + "/sites", this);
 
 	// Complete auto-complete
 	static QStringList specialCompletes = { "grabber:alreadyExists", "grabber:inMd5List", "grabber:downloaded", "grabber:favorited" };
@@ -188,7 +188,7 @@ Profile::~Profile()
 	delete m_commands;
 	delete m_monitorManager;
 	delete m_downloadQueryManager;
-	delete m_urlDownloaderManager;
+	// delete m_urlDownloaderManager;
 }
 
 
