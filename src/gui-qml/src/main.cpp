1ca81a65c1b9d9a0334c89154ffb6836cb1c31f2
Bionus <bio.nus@hotmail.fr>
2020-10-16 18:54:06 +0200

    Copy necessary assets on Android QML startup

--- src/gui-qml/src/main.cpp
+++ src/gui-qml/src/main.cpp
@@ -1,3 +1,4 @@
+#include <QDir>
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
@@ -15,6 +16,16 @@ int main(int argc, char *argv[])
 	app.setApplicationVersion(VERSION);
 	app.setOrganizationName("Bionus");
 	app.setOrganizationDomain("bionus.fr.cr");
+
+	// Copy settings files to writable directory
+	const QStringList toCopy { "sites/", "themes/", "webservices/" };
+	for (const QString &tgt : toCopy) {
+		const QString from = savePath(tgt, true, false);
+		const QString to = savePath(tgt, true, true);
+		if (!QDir(to).exists() && QDir(from).exists()) {
+			copyRecursively(from, to);
+		}
+	}
 
 	const QUrl url(QStringLiteral("qrc:/main-screen.qml"));
 
