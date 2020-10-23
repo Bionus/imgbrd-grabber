7ab86c137a2c35a333fc75b571b3875767470e6c
Jack Vasti <bio.nus@hotmail.fr>
2020-10-24 00:38:59 +0200

    New translations YourLanguage.ts (English)

---
+++ src/gui-qml/languages/English.ts
@@ -0,0 +1,124 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en" sourcelanguage="en">
+  <context>
+    <name>AddSourceScreen</name>
+    <message>
+      <source>Add new source</source>
+      <translation>Add new source</translation>
+    </message>
+    <message>
+      <source>Error</source>
+      <translation>Error</translation>
+    </message>
+    <message>
+      <source>Type</source>
+      <translation>Type</translation>
+    </message>
+    <message>
+      <source>URL</source>
+      <translation>URL</translation>
+    </message>
+    <message>
+      <source>HTTPS</source>
+      <translation>HTTPS</translation>
+    </message>
+    <message>
+      <source>Add</source>
+      <translation>Add</translation>
+    </message>
+  </context>
+  <context>
+    <name>FolderSetting</name>
+    <message>
+      <source>Please choose a directory</source>
+      <translation>Please choose a directory</translation>
+    </message>
+  </context>
+  <context>
+    <name>ImageScreen</name>
+    <message>
+      <source>Image</source>
+      <translation>Image</translation>
+    </message>
+  </context>
+  <context>
+    <name>LogScreen</name>
+    <message>
+      <source>Log</source>
+      <translation>Log</translation>
+    </message>
+  </context>
+  <context>
+    <name>MainDrawer</name>
+    <message>
+      <source>Search</source>
+      <translation>Search</translation>
+    </message>
+    <message>
+      <source>Favorites</source>
+      <translation>Favorites</translation>
+    </message>
+    <message>
+      <source>Log</source>
+      <translation>Log</translation>
+    </message>
+    <message>
+      <source>Settings</source>
+      <translation>Settings</translation>
+    </message>
+  </context>
+  <context>
+    <name>SearchScreen</name>
+    <message>
+      <source>Search</source>
+      <translation>Search</translation>
+    </message>
+    <message>
+      <source>Search...</source>
+      <translation>Search...</translation>
+    </message>
+    <message>
+      <source>Sources</source>
+      <translation>Sources</translation>
+    </message>
+    <message>
+      <source>Go</source>
+      <translation>Go</translation>
+    </message>
+  </context>
+  <context>
+    <name>SettingsScreen</name>
+    <message>
+      <source>Settings</source>
+      <translation>Settings</translation>
+    </message>
+    <message>
+      <source>Save</source>
+      <translation>Save</translation>
+    </message>
+    <message>
+      <source>Filename</source>
+      <translation>Filename</translation>
+    </message>
+    <message>
+      <source>Folder</source>
+      <translation>Folder</translation>
+    </message>
+    <message>
+      <source>Hide blacklisted</source>
+      <translation>Hide blacklisted</translation>
+    </message>
+    <message>
+      <source>Hide blacklisted images from the results.</source>
+      <translation>Hide blacklisted images from the results.</translation>
+    </message>
+  </context>
+  <context>
+    <name>SourcesScreen</name>
+    <message>
+      <source>Sources selection</source>
+      <translation>Sources selection</translation>
+    </message>
+  </context>
+</TS>
