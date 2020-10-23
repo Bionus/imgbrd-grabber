e33a2f4d7ac9b02a6e02078d583deee75b719fc1
Jack Vasti <bio.nus@hotmail.fr>
2020-10-24 00:40:55 +0200

    Add QML translations from Crowdin (#2150)

---
+++ src/gui-qml/languages/French.ts
@@ -0,0 +1,124 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr" sourcelanguage="en">
+  <context>
+    <name>AddSourceScreen</name>
+    <message>
+      <source>Add new source</source>
+      <translation>Ajouter une source</translation>
+    </message>
+    <message>
+      <source>Error</source>
+      <translation>Erreur</translation>
+    </message>
+    <message>
+      <source>Type</source>
+      <translation>Type</translation>
+    </message>
+    <message>
+      <source>URL</source>
+      <translation>URL</translation>
+    </message>
+    <message>
+      <source>HTTPS</source>
+      <translation>HTTPS</translation>
+    </message>
+    <message>
+      <source>Add</source>
+      <translation>Ajouter</translation>
+    </message>
+  </context>
+  <context>
+    <name>FolderSetting</name>
+    <message>
+      <source>Please choose a directory</source>
+      <translation>Choisissez un dossier</translation>
+    </message>
+  </context>
+  <context>
+    <name>ImageScreen</name>
+    <message>
+      <source>Image</source>
+      <translation>Image</translation>
+    </message>
+  </context>
+  <context>
+    <name>LogScreen</name>
+    <message>
+      <source>Log</source>
+      <translation>Log</translation>
+    </message>
+  </context>
+  <context>
+    <name>MainDrawer</name>
+    <message>
+      <source>Search</source>
+      <translation>Recherche</translation>
+    </message>
+    <message>
+      <source>Favorites</source>
+      <translation>Favoris</translation>
+    </message>
+    <message>
+      <source>Log</source>
+      <translation>Log</translation>
+    </message>
+    <message>
+      <source>Settings</source>
+      <translation>Options</translation>
+    </message>
+  </context>
+  <context>
+    <name>SearchScreen</name>
+    <message>
+      <source>Search</source>
+      <translation>Recherche</translation>
+    </message>
+    <message>
+      <source>Search...</source>
+      <translation>Recherche...</translation>
+    </message>
+    <message>
+      <source>Sources</source>
+      <translation>Sources</translation>
+    </message>
+    <message>
+      <source>Go</source>
+      <translation>Go</translation>
+    </message>
+  </context>
+  <context>
+    <name>SettingsScreen</name>
+    <message>
+      <source>Settings</source>
+      <translation>Options</translation>
+    </message>
+    <message>
+      <source>Save</source>
+      <translation>Enregistrer</translation>
+    </message>
+    <message>
+      <source>Filename</source>
+      <translation>Nom de fichier</translation>
+    </message>
+    <message>
+      <source>Folder</source>
+      <translation>Dossier</translation>
+    </message>
+    <message>
+      <source>Hide blacklisted</source>
+      <translation>Cacher la liste noire</translation>
+    </message>
+    <message>
+      <source>Hide blacklisted images from the results.</source>
+      <translation>Masquer des résultats les images sur la liste noire.</translation>
+    </message>
+  </context>
+  <context>
+    <name>SourcesScreen</name>
+    <message>
+      <source>Sources selection</source>
+      <translation>Sélection des sources</translation>
+    </message>
+  </context>
+</TS>
---
+++ src/gui-qml/languages/Polish.ts
@@ -0,0 +1,124 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pl" sourcelanguage="en">
+  <context>
+    <name>AddSourceScreen</name>
+    <message>
+      <source>Add new source</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Error</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Type</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>URL</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>HTTPS</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Add</source>
+      <translation type="unfinished"/>
+    </message>
+  </context>
+  <context>
+    <name>FolderSetting</name>
+    <message>
+      <source>Please choose a directory</source>
+      <translation type="unfinished"/>
+    </message>
+  </context>
+  <context>
+    <name>ImageScreen</name>
+    <message>
+      <source>Image</source>
+      <translation type="unfinished"/>
+    </message>
+  </context>
+  <context>
+    <name>LogScreen</name>
+    <message>
+      <source>Log</source>
+      <translation type="unfinished"/>
+    </message>
+  </context>
+  <context>
+    <name>MainDrawer</name>
+    <message>
+      <source>Search</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Favorites</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Log</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Settings</source>
+      <translation type="unfinished"/>
+    </message>
+  </context>
+  <context>
+    <name>SearchScreen</name>
+    <message>
+      <source>Search</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Search...</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Sources</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Go</source>
+      <translation type="unfinished"/>
+    </message>
+  </context>
+  <context>
+    <name>SettingsScreen</name>
+    <message>
+      <source>Settings</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Save</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Filename</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Folder</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Hide blacklisted</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
+      <source>Hide blacklisted images from the results.</source>
+      <translation type="unfinished"/>
+    </message>
+  </context>
+  <context>
+    <name>SourcesScreen</name>
+    <message>
+      <source>Sources selection</source>
+      <translation type="unfinished"/>
+    </message>
+  </context>
+</TS>
