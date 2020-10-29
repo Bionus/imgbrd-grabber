247a8a5c51a4efdaf880a20ccd2853da3af9f3e1
Bionus <bio.nus@hotmail.fr>
2020-10-29 12:28:25 +0100

    Also copy CLI binary in package.sh (issue #2161)

--- scripts/package.sh
+++ scripts/package.sh
@@ -6,9 +6,12 @@
 APP_DIR=$1
 mkdir -p $APP_DIR
 
-# Copy all required files to the release directory
-cp "build/gui/Grabber" $APP_DIR
+# Copy built files
+cp "build/gui/Grabber" $APP_DIR 2> /dev/null
+cp "build/cli/Grabber-cli" $APP_DIR 2> /dev/null
+cp build/languages/*.qm "$APP_DIR/languages/" 2> /dev/null
+
+# Copy other required files to the release directory
 rsync -ar --exclude="node_modules" --exclude="CMakeLists.txt" --exclude="package*.json" --exclude="*.config.js" --exclude="tsconfig.json" --exclude="tslint.json" --exclude="**/*.ts" --exclude="**/resources" --exclude="Sankaku" --exclude="Tumblr" src/sites $APP_DIR
 cp -r src/dist/common/* $APP_DIR
-cp build/languages/*.qm "$APP_DIR/languages/"
 touch "$APP_DIR/settings.ini"
