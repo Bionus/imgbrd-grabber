f3d04dd94242420d2858789d9ed8f334689b990c
Bionus <bio.nus@hotmail.fr>
2020-11-18 03:09:26 +0100

    Fix 'make install' command on Linux not properly copying resources

--- scripts/package-appimage.sh
+++ scripts/package-appimage.sh
@@ -7,27 +7,10 @@ set -e
 APP_DIR="release"
 mkdir -p $APP_DIR
 
-# Application binary
-mkdir -p $APP_DIR/usr/bin
-cp "build/gui/Grabber" $APP_DIR/usr/bin/grabber
-
-# Desktop file
-mkdir -p $APP_DIR/usr/share/applications
-cp "src/dist/linux/grabber.desktop" $APP_DIR/usr/share/applications
-
-# AppData file
-mkdir -p $APP_DIR/usr/share/metainfo
-cp "src/dist/linux/grabber.appdata.xml" $APP_DIR/usr/share/metainfo
-
-# Icon
-mkdir -p $APP_DIR/usr/share/icons/hicolor
-cp "src/dist/linux/grabber.png" $APP_DIR/usr/share/icons/hicolor
-
-# Copy other files in $PREFIX/share/Grabber where they can be found
-mkdir -p $APP_DIR/usr/share/Grabber
-rsync -ar --exclude="node_modules" --exclude="CMakeLists.txt" --exclude="package*.json" --exclude="*.config.js" --exclude="tsconfig.json" --exclude="tslint.json" --exclude="**/*.ts" --exclude="**/resources" --exclude="Sankaku" --exclude="Tumblr" src/sites $APP_DIR/usr/share/Grabber
-cp -r src/dist/common/* $APP_DIR/usr/share/Grabber
-touch "$APP_DIR/usr/share/Grabber/settings.ini"
+# "make install" into the app directory
+pushd build
+    make DESTDIR=../$APP_DIR install
+popd
 
 # See https://github.com/probonopd/linuxdeployqt
 export VERSION=$GRABBER_VERSION
