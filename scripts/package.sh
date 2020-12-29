f7385d5c680b37d54e1d32ab268b61f06c7a7c35
Bionus <bio.nus@hotmail.fr>
2020-12-29 17:22:29 +0100

    Don't include everything from src/sites in Android build (issue #2147)

--- scripts/package.sh
+++ scripts/package.sh
@@ -12,6 +12,6 @@ cp "build/cli/Grabber-cli" $APP_DIR 2> /dev/null
 cp build/languages/*.qm "$APP_DIR/languages/" 2> /dev/null
 
 # Copy other required files to the release directory
-rsync -ar --exclude="node_modules" --exclude="CMakeLists.txt" --exclude="package*.json" --exclude="*.config.js" --exclude="tsconfig.json" --exclude="tslint.json" --exclude="**/*.ts" --exclude="**/resources" --exclude="Sankaku" --exclude="Tumblr" src/sites $APP_DIR
+rsync -ar --exclude-from="src/sites/exclude.txt" src/sites $APP_DIR
 cp -r src/dist/common/* $APP_DIR
 touch "$APP_DIR/settings.ini"
