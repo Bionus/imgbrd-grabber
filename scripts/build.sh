1d4102b939a4f779c976832f482d090a8c22f9fb
Penguin-Guru <Penguin-Guru@ProtonMail.com>
2021-03-09 14:50:41 -0800

    In retrospect, this is probably what you wanted.

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,6 +6,6 @@ mkdir -p build
 declare -i ret=0
 pushd build
     cmake ../src || ret=1
-    if ((!ret)); then make "$@" -j$(($(nproc)+1)) || ret=2; fi
+    if ((!ret)); then make $@ -j$(($(nproc)+1)) || ret=2; fi
 popd
 exit "$ret"
