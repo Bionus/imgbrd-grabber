f04b650ce78d41446481a5798d626a9110856d47
Penguin-Guru <Penguin-Guru@ProtonMail.com>
2021-03-09 13:09:34 -0800

    Added support for more than two distros.

--- build.sh
+++ build.sh
@@ -4,14 +4,36 @@
 git submodule update --init --recursive
 
 # Install required packages
-if type pacman > /dev/null 2>&1
-then
-  sudo pacman -Sy
-  sudo pacman -S "qt" "gcc" "cmake" "libpulse" "nodejs" "npm"
-else
-  sudo apt-get install -qq "qtbase5-dev" "qtscript5-dev" "qtmultimedia5-dev" "qtdeclarative5-dev" "qttools5-dev" "qttools5-dev-tools"
-  sudo apt-get install -qq "g++" "cmake" "libssl-dev" "nodejs" "npm"
+no_pm=1
+install_failed=1
+for pm in pacman apt-get emerge; do
+	if type "$pm" > /dev/null 2>&1 ; then
+		no_pm=0
+		case "$pm" in
+			pacman)
+				sudo pacman -Sy
+				sudo pacman -S "qt" "gcc" "cmake" "libpulse" "nodejs" "npm" && install_failed=0
+				;;
+			apt-get)
+				sudo apt-get install -qq "qtbase5-dev" "qtscript5-dev" "qtmultimedia5-dev" "qtdeclarative5-dev" "qttools5-dev" "qttools5-dev-tools" || continue
+				sudo apt-get install -qq "g++" "cmake" "libssl-dev" "nodejs" "npm" && install_failed=0
+				;;
+			emerge)
+				## Dependencies untested!
+				sudo emerge --no-replace --ask --verbose \
+					"dev-qt/qtcore" "dev-qt/qtscript" "dev-qt/qtmultimedia" "dev-qt/qtdeclarative" \
+					"sys-devel/gcc" "dev-util/cmake" "net-libs/nodejs" \
+				&& install_failed=0
+				;;
+		esac
+		if [ "$install_failed" -eq 0 ]; then break; fi
+	fi
+done
+if [ "$no_pm" -eq 1 ]; then
+	echo "Failed to detect your distribution's package manager."
+	exit 1
 fi
+if [ "$install_failed" -eq 1 ]; then exit 2; fi
 
 # Build the project in the build directory
 ./scripts/build.sh ${1:-gui translations}
