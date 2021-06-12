35082032de9f2964226648236488e1518cb43b5b
Bionus <bio.nus@hotmail.fr>
2021-06-12 17:22:12 +0200

    Load rule34.xxx tags when generating tag databases (issue #2384)

--- scripts/generate-tag-databases.sh
+++ scripts/generate-tag-databases.sh
@@ -6,6 +6,7 @@ set -e
 DESTDIR="databases"
 SOURCES=(
     "Gelbooru (0.2)/gelbooru.com"
+    "Gelbooru (0.2)/rule34.xxx"
 )
 MIN_TAG_COUNT=100
 
@@ -18,6 +19,11 @@ for FULL in "${SOURCES[@]}"
 do
     SOURCE=${FULL%%/*}
     SITE=${FULL##*/}
+
+    echo "Loading tags for $SITE ($SOURCE)..."
+
+    mkdir -p "sites/$SOURCE/$SITE"
+    touch "sites/$SOURCE/$SITE/tags.db"
 
     ./Grabber-cli --load-tag-database --sources $SITE --tags-min $MIN_TAG_COUNT
     cp "sites/$SOURCE/$SITE/tags.db" "../$DESTDIR/$SITE ($MIN_TAG_COUNT).db"
