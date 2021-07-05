a1516082cda2fe660f64916f70b43896344f7331
Bionus <bio.nus@hotmail.fr>
2021-07-06 00:46:11 +0200

    Mark a few strings as non-translatable

--- src/languages/YourLanguage.ts
+++ src/languages/YourLanguage.ts
@@ -930,11 +930,6 @@ Please solve the issue before resuming the download.</source>
       <translation type="unfinished"/>
     </message>
     <message>
-      <location filename="../gui/src/favorite-window.ui" line="231"/>
-      <source>...</source>
-      <translation type="unfinished"/>
-    </message>
-    <message>
       <location filename="../gui/src/favorite-window.ui" line="246"/>
       <source>Delete</source>
       <translation type="unfinished"/>
@@ -1182,19 +1177,6 @@ Please solve the issue before resuming the download.</source>
     <message>
       <location filename="../gui/src/tabs/gallery-tab.ui" line="339"/>
       <source>Get &amp;all</source>
-      <translation type="unfinished"/>
-    </message>
-  </context>
-  <context>
-    <name>GifPlayer</name>
-    <message>
-      <location filename="../gui/src/viewer/players/gif-player.ui" line="14"/>
-      <source>Form</source>
-      <translation type="unfinished"/>
-    </message>
-    <message>
-      <location filename="../gui/src/viewer/players/gif-player.ui" line="74"/>
-      <source>00:00 / 00:00</source>
       <translation type="unfinished"/>
     </message>
   </context>
@@ -1483,11 +1465,6 @@ Please solve the issue before resuming the download.</source>
       <translation type="unfinished"/>
     </message>
     <message>
-      <location filename="../gui/src/settings/log-window.ui" line="147"/>
-      <source>...</source>
-      <translation type="unfinished"/>
-    </message>
-    <message>
       <location filename="../gui/src/settings/log-window.ui" line="179"/>
       <source>&lt;i&gt;Each time an image is saved, an external text file will be save with the same name at the same location.&lt;/i&gt;</source>
       <translation type="unfinished"/>
@@ -1729,11 +1706,6 @@ Please solve the issue before resuming the download.</source>
       <translation type="unfinished"/>
     </message>
     <message>
-      <location filename="../gui/src/utils/md5-database-converter/md5-database-converter.ui" line="56"/>
-      <source>%v</source>
-      <translation type="unfinished"/>
-    </message>
-    <message>
       <location filename="../gui/src/utils/md5-database-converter/md5-database-converter.ui" line="78"/>
       <source>Start</source>
       <translation type="unfinished"/>
@@ -1876,11 +1848,6 @@ Please solve the issue before resuming the download.</source>
     <message>
       <location filename="../gui/src/monitor-window.ui" line="56"/>
       <source>Last check</source>
-      <translation type="unfinished"/>
-    </message>
-    <message>
-      <location filename="../gui/src/monitor-window.ui" line="69"/>
-      <source>yyyy/MM/dd HH:mm:ss</source>
       <translation type="unfinished"/>
     </message>
     <message>
@@ -4586,11 +4553,6 @@ Please solve the issue before resuming the download.</source>
       <translation type="unfinished"/>
     </message>
     <message>
-      <location filename="../gui/src/sources/sources-window.ui" line="99"/>
-      <source>...</source>
-      <translation type="unfinished"/>
-    </message>
-    <message>
       <location filename="../gui/src/sources/sources-window.ui" line="174"/>
       <source>Add</source>
       <translation type="unfinished"/>
@@ -4657,11 +4619,6 @@ Please solve the issue before resuming the download.</source>
     <message>
       <location filename="../gui/src/settings/start-window.ui" line="69"/>
       <source>Format</source>
-      <translation type="unfinished"/>
-    </message>
-    <message>
-      <location filename="../gui/src/settings/start-window.ui" line="93"/>
-      <source>...</source>
       <translation type="unfinished"/>
     </message>
     <message>
@@ -4781,11 +4738,6 @@ Please solve the issue before resuming the download.</source>
     <message>
       <location filename="../gui/src/utils/tag-loader/tag-loader.ui" line="14"/>
       <source>Tag loader</source>
-      <translation type="unfinished"/>
-    </message>
-    <message>
-      <location filename="../gui/src/utils/tag-loader/tag-loader.ui" line="55"/>
-      <source>%v</source>
       <translation type="unfinished"/>
     </message>
     <message>
@@ -4980,11 +4932,6 @@ Please solve the issue before resuming the download.</source>
   <context>
     <name>TokenSettingsWidget</name>
     <message>
-      <location filename="../gui/src/settings/token-settings-widget.ui" line="14"/>
-      <source>Form</source>
-      <translation type="unfinished"/>
-    </message>
-    <message>
       <location filename="../gui/src/settings/token-settings-widget.ui" line="20"/>
       <source>If empty</source>
       <translation type="unfinished"/>
@@ -5065,19 +5012,6 @@ Please solve the issue before resuming the download.</source>
     <message>
       <location filename="../gui/src/updater/update-dialog.cpp" line="60"/>
       <source>Version &lt;b&gt;%1&lt;/b&gt;</source>
-      <translation type="unfinished"/>
-    </message>
-  </context>
-  <context>
-    <name>VideoPlayer</name>
-    <message>
-      <location filename="../gui/src/viewer/players/video-player.ui" line="14"/>
-      <source>Form</source>
-      <translation type="unfinished"/>
-    </message>
-    <message>
-      <location filename="../gui/src/viewer/players/video-player.ui" line="61"/>
-      <source>00:00 / 00:00</source>
       <translation type="unfinished"/>
     </message>
   </context>
