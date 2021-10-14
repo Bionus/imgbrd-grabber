574de83bf4b1f72e663d939b52db88759eab5f8f
Penguin-Guru <Penguin-Guru@ProtonMail.com>
2021-10-14 10:33:20 -0700

    Removed failed attempts at constexpr default QStrings.

--- src/gui/src/custom-buttons/state.h
+++ src/gui/src/custom-buttons/state.h
@@ -4,13 +4,6 @@
 #include <QString>
 
 
-// https://stackoverflow.com/questions/56201976/qt-vs-constexpr-string-literal
-/*struct ConstLatin1String : public QLatin1String
-{
-    constexpr ConstLatin1String(const char* const s) :
-        QLatin1String(s, static_cast<int>(std::char_traits<char>::length(s))) {}
-};*/
-
 class ZoomWindow;
 template <typename scope = ZoomWindow>
 using ButtonEffect = void (scope::*)();
@@ -19,14 +12,6 @@ using ButtonEffect = void (scope::*)();
 //template <typename scope = ZoomWindow, typename R = void, typename ...params>
 //using ButtonEffect = R (scope::*)(params...);
 
-/*// https://www.tutorialfor.com/questions-144170.htm
-class QString
-{
-	public:
-		constexpr QString (char * str) {mStr = str;}
-		char * mStr = 0;
-};*/
-
 class ButtonState
 {
 	public:
@@ -34,12 +19,6 @@ class ButtonState
 		QString text;
 		QString toolTip = "";
 		ButtonEffect<> function = nullptr;
-
-		//constexpr ButtonState(unsigned short type, const char *text, const char *toolTip) : type(type), text(text), toolTip(toolTip){}
-		//constexpr ButtonState(const unsigned short type, const QString text, const QString toolTip) : type(type), text(text), toolTip(toolTip){}
-		//constexpr ButtonState(unsigned short type, ConstLatin1String text, ConstLatin1String toolTip) : type(type), text(text), toolTip(toolTip){}
-		//constexpr ButtonState(const unsigned short type, std::string text, std::string toolTip) : type(type), text(text), toolTip(toolTip){}
-		ButtonState(unsigned short type, QString text, QString toolTip) : type(type), text(std::move(text)), toolTip(std::move(toolTip)) {}
 
 		// QSettings serialization
 		ButtonState() {}
