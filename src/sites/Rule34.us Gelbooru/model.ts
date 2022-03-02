1ca19ad6e0cb0370bd4612be4d6713f7ae8a53b1
GiovanH <GiovanH@users.noreply.github.com>
2022-03-02 17:16:36 -0600

    Add rule34.us site format (#2606)

---
+++ src/sites/Rule34.us Gelbooru/model.ts
@@ -0,0 +1,64 @@
+function completeImage(img: IImage): IImage {
+    if ((!img.file_url || img.file_url.length < 5) && img.preview_url) {
+        img.file_url = img.preview_url
+            .replace("/thumbnails/", "/images/")
+            .replace("/thumbnail_", "/");
+    }
+
+    return img;
+}
+
+export const source: ISource = {
+    name: "Modified Gelbooru (0.1)",
+    modifiers: ["rating:safe", "rating:questionable", "rating:explicit", "user:", "fav:", "fastfav:", "md5:", "source:", "id:", "width:", "height:", "score:", "mpixels:", "filesize:", "date:", "gentags:", "arttags:", "chartags:", "copytags:", "approver:", "parent:", "sub:", "order:id", "order:id_desc", "order:score", "order:score_asc", "order:mpixels", "order:mpixels_asc", "order:filesize", "order:landscape", "order:portrait", "order:favcount", "order:rank", "parent:none", "unlocked:rating"],
+    tagFormat: {
+        case: "lower",
+        wordSeparator: "_",
+    },
+    searchFormat: {
+        and: " ",
+    },
+    auth: {},
+    apis: {
+        html: {
+            name: "Regex",
+            auth: [],
+            forcedLimit: 20,
+            search: {
+                url: (query: ISearchQuery): string => {
+                    const page: number = query.page - 1;
+                    const search = query.search && query.search.length > 0 ? encodeURIComponent(query.search) : "all";
+                    return "/index.php?r=posts/index&q=" + search + (page == 0 ? '' : "&page=" + page);
+                },
+                parse: (src: string): IParsedSearch => {
+                    const pageCount = Grabber.regexToConst("page", '<div class="pagination">\\W*(<a.*>.*<\\/a>\\W*)*(<a href="[^"]+page=(?<page>\\d+)".*>.*<\\/a>\\W*)<\\/div>', src);
+                    return {
+                        images: Grabber.regexToImages('<a id="(?<id>\\d+)" href="[^"]+"><img +src="(?<preview_url>[^"]*thumbnail_(?<md5>[^.]+)\\.[^"]+)" title=" *(?<tags>[^"]*)" .+?</a></div>', src).map(completeImage),
+                        pageCount: pageCount ? (parseInt(pageCount, 10) + 1) : undefined,
+                        tags: Grabber.regexToTags('<li><a[^>]*>\\+</a><a [^>]*>-</a> <span [^>]*>\\? <a href="[^"]*">(?<name>[^<]+)</a> (?<count>\\d+)</span></li>', src),
+                    };
+                },
+            },
+            details: {
+                url: (id: string, md5: string): string => {
+                    return "/index.php?r=posts/view&id=" + id;
+                },
+                parse: (src: string): IParsedDetails => {
+                    return {
+                        tags: Grabber.regexToTags('<li><a[^>]*>\\+</a><a [^>]*>-</a> <span [^>]*>\\? <a href="[^"]*">(?<name>[^<]+)</a> (?<count>\\d+)</span></li>', src),
+                        imageUrl: Grabber.regexToConst("url", '<img[^>]+src="([^"]+)"[^>]+onclick="Note\\.toggle\\(\\);"[^>]*/>', src),
+                    };
+                },
+            },
+            check: {
+                url: (): string => {
+                    return "/";
+                },
+                parse: (src: string): boolean => {
+                    return src.search(/Running modified Gelbooru(?: Beta)? 0\.1/) !== -1
+                        || src.search(/Running modified <a[^>]*>Gelbooru<\/a>(?: Beta)? 0\.1/) !== -1;
+                },
+            },
+        },
+    },
+};
