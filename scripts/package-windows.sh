311de6b539bd2efeeac3b814e01cef7a32955734
Bionus <bio.nus@hotmail.fr>
2022-04-15 01:38:17 +0200

    Fix windows paths when using package-windows.sh

--- scripts/package-windows.sh
+++ scripts/package-windows.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 # Package everything into a "Grabber.zip" file at the root of the git repository
 
+# Helper to fix Windows env variables to work in bash
+function fixpath() {
+    line=$1
+    line=${line/C://c}
+    line=${line/D://d}
+    line=${line//\\//}
+    echo "$line"
+}
+
 APP_DIR="release"
 
 # Copy all required files to the release directory
@@ -8,12 +17,12 @@ APP_DIR="release"
 cp -r src/dist/windows/* $APP_DIR
 
 # Add Qt DLL and files
-$Qt5_Dir/bin/windeployqt.exe --dir $APP_DIR "$APP_DIR/Grabber.exe"
+$(fixpath $Qt5_Dir)/bin/windeployqt.exe --dir $APP_DIR "$APP_DIR/Grabber.exe"
 
 # Add OpenSSL and MySQL DLL
-cp $OPENSSL_ROOT_DIR/libcrypto-1_1*.dll "$APP_DIR"
-cp $OPENSSL_ROOT_DIR/libssl-1_1*.dll "$APP_DIR"
-cp $MYSQL_DRIVER_DIR/libmysql.dll "$APP_DIR"
+cp $(fixpath $OPENSSL_ROOT_DIR)/libcrypto-1_1*.dll "$APP_DIR"
+cp $(fixpath $OPENSSL_ROOT_DIR)/libssl-1_1*.dll "$APP_DIR"
+cp $(fixpath $MYSQL_DRIVER_DIR)/libmysql.dll "$APP_DIR"
 
 # Zip the whole directory
 pushd $APP_DIR
