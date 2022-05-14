36f950d824876e7570c402d8ef18a971fed2df49
Bionus <bio.nus@hotmail.fr>
2022-05-14 18:31:26 +0200

    Add comments to Login and LoginFactory classes

--- src/lib/src/login/login-factory.h
+++ src/lib/src/login/login-factory.h
@@ -7,9 +7,20 @@ class NetworkManager;
 class Site;
 
 
+/**
+ * Factory to build Login instances.
+ */
 class LoginFactory
 {
 	public:
+		/**
+		 * Build a Login from an Auth object.
+		 *
+		 * @param site The site to build the Login instance for.
+		 * @param auth The Auth instance containing all necessary information to build the Login instance.
+		 * @param manager The network manager to use in the Login class for HTTP requests.
+		 * @return A new Login instance on success, `nullptr` on failure.
+		 */
 		static Login *build(Site *site, Auth *auth, NetworkManager *manager);
 };
 
