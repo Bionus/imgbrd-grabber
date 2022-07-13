60597ff1d68a3fcc56ba251c3ab8e9476687e905
Bionus <bio.nus@hotmail.fr>
2022-07-14 01:15:58 +0200

    Make default button names translatable (fix #2705)

--- src/gui/src/viewer/viewer-window-buttons.h
+++ src/gui/src/viewer/viewer-window-buttons.h
@@ -23,40 +23,40 @@ namespace ViewerWindowButtons
 
 	const ButtonState DefaultPrevState(0, QStringLiteral("<"), QStringLiteral("Previous search result"));
 	const ButtonState DefaultNextState(0, QStringLiteral(">"), QStringLiteral("Next search result"));
-	const ButtonState DefaultDetailsState(0, QStringLiteral("Details"), QStringLiteral("Media details"));
-	const ButtonState DefaultSaveAsState(0, QStringLiteral("Save as..."), QStringLiteral("Save to irregular location"));
+	const ButtonState DefaultDetailsState(0, QObject::tr("Details"), QStringLiteral("Media details"));
+	const ButtonState DefaultSaveAsState(0, QObject::tr("Save as..."), QStringLiteral("Save to irregular location"));
 
-	const ButtonState DefaultSaveStateSave(SaveState::Save, QStringLiteral("Save"), QStringLiteral("Save to usual location"));
-	const ButtonState DefaultSaveStateSaving(SaveState::Saving, QStringLiteral("Saving..."), QStringLiteral(""));
-	const ButtonState DefaultSaveStateSaved(SaveState::Saved, QStringLiteral("Saved!"), QStringLiteral(""));
-	const ButtonState DefaultSaveStateCopied(SaveState::Copied, QStringLiteral("Copied!"), QStringLiteral(""));
-	const ButtonState DefaultSaveStateMoved(SaveState::Moved, QStringLiteral("Moved!"), QStringLiteral(""));
-	const ButtonState DefaultSaveStateLinked(SaveState::Linked, QStringLiteral("Link created!"), QStringLiteral(""));
-	const ButtonState DefaultSaveStateExistsMd5(SaveState::ExistsMd5, QStringLiteral("MD5 already exists"), QStringLiteral(""));
-	const ButtonState DefaultSaveStateExistsDisk(SaveState::ExistsDisk, QStringLiteral("Already saved"), QStringLiteral(""));
-	const ButtonState DefaultSaveStateDelete(SaveState::Delete, QStringLiteral("Delete"), QStringLiteral(""));
+	const ButtonState DefaultSaveStateSave(SaveState::Save, QObject::tr("Save"), QStringLiteral("Save to usual location"));
+	const ButtonState DefaultSaveStateSaving(SaveState::Saving, QObject::tr("Saving..."), QStringLiteral(""));
+	const ButtonState DefaultSaveStateSaved(SaveState::Saved, QObject::tr("Saved!"), QStringLiteral(""));
+	const ButtonState DefaultSaveStateCopied(SaveState::Copied, QObject::tr("Copied!"), QStringLiteral(""));
+	const ButtonState DefaultSaveStateMoved(SaveState::Moved, QObject::tr("Moved!"), QStringLiteral(""));
+	const ButtonState DefaultSaveStateLinked(SaveState::Linked, QObject::tr("Link created!"), QStringLiteral(""));
+	const ButtonState DefaultSaveStateExistsMd5(SaveState::ExistsMd5, QObject::tr("MD5 already exists"), QStringLiteral(""));
+	const ButtonState DefaultSaveStateExistsDisk(SaveState::ExistsDisk, QObject::tr("Already saved"), QStringLiteral(""));
+	const ButtonState DefaultSaveStateDelete(SaveState::Delete, QObject::tr("Delete"), QStringLiteral(""));
 
-	const ButtonState DefaultSaveNQuitStateSave(SaveState::Save, QStringLiteral("Save & close"), QStringLiteral("Save to usual location and close window"));
-	const ButtonState DefaultSaveNQuitStateSaving(SaveState::Saving, QStringLiteral("Saving..."), QStringLiteral(""));
-	const ButtonState DefaultSaveNQuitStateClose(2, QStringLiteral("Close"), QStringLiteral("")); // Consider adding this to SaveButtonState.
+	const ButtonState DefaultSaveNQuitStateSave(SaveState::Save, QObject::tr("Save & close"), QStringLiteral("Save to usual location and close window"));
+	const ButtonState DefaultSaveNQuitStateSaving(SaveState::Saving, QObject::tr("Saving..."), QStringLiteral(""));
+	const ButtonState DefaultSaveNQuitStateClose(2, QObject::tr("Close"), QStringLiteral("")); // Consider adding this to SaveButtonState.
 
-	const ButtonState DefaultOpenState(0, QStringLiteral("Open"), QStringLiteral("Open usual save location in new window"));
+	const ButtonState DefaultOpenState(0, QObject::tr("Open"), QStringLiteral("Open usual save location in new window"));
 
-	const ButtonState DefaultSaveFavStateSave(SaveState::Save, QStringLiteral("Save (fav)"), QStringLiteral("Save to favourite location"));
-	const ButtonState DefaultSaveFavStateSaving(SaveState::Saving, QStringLiteral("Saving... (fav)"), QStringLiteral(""));
-	const ButtonState DefaultSaveFavStateSaved(SaveState::Saved, QStringLiteral("Saved! (fav)"), QStringLiteral(""));
-	const ButtonState DefaultSaveFavStateCopied(SaveState::Copied, QStringLiteral("Copied! (fav)"), QStringLiteral(""));
-	const ButtonState DefaultSaveFavStateMoved(SaveState::Moved, QStringLiteral("Moved! (fav)"), QStringLiteral(""));
-	const ButtonState DefaultSaveFavStateLinked(SaveState::Linked, QStringLiteral("Link created! (fav)"), QStringLiteral(""));
-	const ButtonState DefaultSaveFavStateExistsMd5(SaveState::ExistsMd5, QStringLiteral("MD5 already exists (fav)"), QStringLiteral(""));
-	const ButtonState DefaultSaveFavStateExistsDisk(SaveState::ExistsDisk, QStringLiteral("Already saved (fav)"), QStringLiteral(""));
-	const ButtonState DefaultSaveFavStateDelete(SaveState::Delete, QStringLiteral("Delete (fav)"), QStringLiteral(""));
+	const ButtonState DefaultSaveFavStateSave(SaveState::Save, QObject::tr("Save (fav)"), QStringLiteral("Save to favourite location"));
+	const ButtonState DefaultSaveFavStateSaving(SaveState::Saving, QObject::tr("Saving... (fav)"), QStringLiteral(""));
+	const ButtonState DefaultSaveFavStateSaved(SaveState::Saved, QObject::tr("Saved! (fav)"), QStringLiteral(""));
+	const ButtonState DefaultSaveFavStateCopied(SaveState::Copied, QObject::tr("Copied! (fav)"), QStringLiteral(""));
+	const ButtonState DefaultSaveFavStateMoved(SaveState::Moved, QObject::tr("Moved! (fav)"), QStringLiteral(""));
+	const ButtonState DefaultSaveFavStateLinked(SaveState::Linked, QObject::tr("Link created! (fav)"), QStringLiteral(""));
+	const ButtonState DefaultSaveFavStateExistsMd5(SaveState::ExistsMd5, QObject::tr("MD5 already exists (fav)"), QStringLiteral(""));
+	const ButtonState DefaultSaveFavStateExistsDisk(SaveState::ExistsDisk, QObject::tr("Already saved (fav)"), QStringLiteral(""));
+	const ButtonState DefaultSaveFavStateDelete(SaveState::Delete, QObject::tr("Delete (fav)"), QStringLiteral(""));
 
-	const ButtonState DefaultSaveNQuitFavStateSave(SaveState::Save, QStringLiteral("Save & close (fav)"), QStringLiteral("Save to favourite location and close window"));
-	const ButtonState DefaultSaveNQuitFavStateSaving(SaveState::Saving, QStringLiteral("Saving... (fav)"), QStringLiteral(""));
-	const ButtonState DefaultSaveNQuitFavStateClose(2, QStringLiteral("Close (fav)"), QStringLiteral("")); // Consider adding this to SaveButtonState.
+	const ButtonState DefaultSaveNQuitFavStateSave(SaveState::Save, QObject::tr("Save & close (fav)"), QStringLiteral("Save to favourite location and close window"));
+	const ButtonState DefaultSaveNQuitFavStateSaving(SaveState::Saving, QObject::tr("Saving... (fav)"), QStringLiteral(""));
+	const ButtonState DefaultSaveNQuitFavStateClose(2, QObject::tr("Close (fav)"), QStringLiteral("")); // Consider adding this to SaveButtonState.
 
-	const ButtonState DefaultOpenFavState(0, QStringLiteral("Open (fav)"), QStringLiteral("Open favourite save location in new window"));
+	const ButtonState DefaultOpenFavState(0, QObject::tr("Open (fav)"), QStringLiteral("Open favourite save location in new window"));
 
 
 	const QMap<unsigned short, ButtonState> DefaultStates {
--- src/gui/src/viewer/viewer-window.cpp
+++ src/gui/src/viewer/viewer-window.cpp
@@ -772,8 +772,8 @@ void ViewerWindow::setButtonState(bool fav, SaveButtonState state)
 
 		// Update button text
 		const ButtonState &defaultState = ViewerWindowButtons::DefaultStates.value(button->type);
-		button->pointer->setText(tr( QString(newState->text.isEmpty() ? defaultState.text : newState->text).replace("&", "&&").toStdString().c_str()));
-		button->pointer->setToolTip(tr((newState->toolTip.isEmpty() ? defaultState.toolTip : newState->toolTip).toStdString().c_str()));
+		button->pointer->setText( QString(newState->text.isEmpty() ? defaultState.text : newState->text).replace("&", "&&").toStdString().c_str());
+		button->pointer->setToolTip((newState->toolTip.isEmpty() ? defaultState.toolTip : newState->toolTip).toStdString().c_str());
 
 		// Connect button to its new action
 		if (newState->function == nullptr) {
