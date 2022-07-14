d60a37f21110f178e5cfaec64043402c85352ad6
Bionus <bio.nus@hotmail.fr>
2022-07-14 13:37:28 +0200

    Add button tooltips to translatable strings (issue #2705)

--- src/gui/src/viewer/viewer-window-buttons.h
+++ src/gui/src/viewer/viewer-window-buttons.h
@@ -21,12 +21,12 @@ namespace ViewerWindowButtons
 		Delete
 	};
 
-	const ButtonState DefaultPrevState(0, QStringLiteral("<"), QStringLiteral("Previous search result"));
-	const ButtonState DefaultNextState(0, QStringLiteral(">"), QStringLiteral("Next search result"));
-	const ButtonState DefaultDetailsState(0, QObject::tr("Details"), QStringLiteral("Media details"));
-	const ButtonState DefaultSaveAsState(0, QObject::tr("Save as..."), QStringLiteral("Save to irregular location"));
+	const ButtonState DefaultPrevState(0, QStringLiteral("<"), QObject::tr("Previous search result"));
+	const ButtonState DefaultNextState(0, QStringLiteral(">"), QObject::tr("Next search result"));
+	const ButtonState DefaultDetailsState(0, QObject::tr("Details"), QObject::tr("Media details"));
+	const ButtonState DefaultSaveAsState(0, QObject::tr("Save as..."), QObject::tr("Save to irregular location"));
 
-	const ButtonState DefaultSaveStateSave(SaveState::Save, QObject::tr("Save"), QStringLiteral("Save to usual location"));
+	const ButtonState DefaultSaveStateSave(SaveState::Save, QObject::tr("Save"), QObject::tr("Save to usual location"));
 	const ButtonState DefaultSaveStateSaving(SaveState::Saving, QObject::tr("Saving..."), QStringLiteral(""));
 	const ButtonState DefaultSaveStateSaved(SaveState::Saved, QObject::tr("Saved!"), QStringLiteral(""));
 	const ButtonState DefaultSaveStateCopied(SaveState::Copied, QObject::tr("Copied!"), QStringLiteral(""));
@@ -36,13 +36,13 @@ namespace ViewerWindowButtons
 	const ButtonState DefaultSaveStateExistsDisk(SaveState::ExistsDisk, QObject::tr("Already saved"), QStringLiteral(""));
 	const ButtonState DefaultSaveStateDelete(SaveState::Delete, QObject::tr("Delete"), QStringLiteral(""));
 
-	const ButtonState DefaultSaveNQuitStateSave(SaveState::Save, QObject::tr("Save & close"), QStringLiteral("Save to usual location and close window"));
+	const ButtonState DefaultSaveNQuitStateSave(SaveState::Save, QObject::tr("Save & close"), QObject::tr("Save to usual location and close window"));
 	const ButtonState DefaultSaveNQuitStateSaving(SaveState::Saving, QObject::tr("Saving..."), QStringLiteral(""));
 	const ButtonState DefaultSaveNQuitStateClose(2, QObject::tr("Close"), QStringLiteral("")); // Consider adding this to SaveButtonState.
 
-	const ButtonState DefaultOpenState(0, QObject::tr("Open"), QStringLiteral("Open usual save location in new window"));
+	const ButtonState DefaultOpenState(0, QObject::tr("Open"), QObject::tr("Open usual save location in new window"));
 
-	const ButtonState DefaultSaveFavStateSave(SaveState::Save, QObject::tr("Save (fav)"), QStringLiteral("Save to favourite location"));
+	const ButtonState DefaultSaveFavStateSave(SaveState::Save, QObject::tr("Save (fav)"), QObject::tr("Save to favourite location"));
 	const ButtonState DefaultSaveFavStateSaving(SaveState::Saving, QObject::tr("Saving... (fav)"), QStringLiteral(""));
 	const ButtonState DefaultSaveFavStateSaved(SaveState::Saved, QObject::tr("Saved! (fav)"), QStringLiteral(""));
 	const ButtonState DefaultSaveFavStateCopied(SaveState::Copied, QObject::tr("Copied! (fav)"), QStringLiteral(""));
@@ -52,11 +52,11 @@ namespace ViewerWindowButtons
 	const ButtonState DefaultSaveFavStateExistsDisk(SaveState::ExistsDisk, QObject::tr("Already saved (fav)"), QStringLiteral(""));
 	const ButtonState DefaultSaveFavStateDelete(SaveState::Delete, QObject::tr("Delete (fav)"), QStringLiteral(""));
 
-	const ButtonState DefaultSaveNQuitFavStateSave(SaveState::Save, QObject::tr("Save & close (fav)"), QStringLiteral("Save to favourite location and close window"));
+	const ButtonState DefaultSaveNQuitFavStateSave(SaveState::Save, QObject::tr("Save & close (fav)"), QObject::tr("Save to favourite location and close window"));
 	const ButtonState DefaultSaveNQuitFavStateSaving(SaveState::Saving, QObject::tr("Saving... (fav)"), QStringLiteral(""));
 	const ButtonState DefaultSaveNQuitFavStateClose(2, QObject::tr("Close (fav)"), QStringLiteral("")); // Consider adding this to SaveButtonState.
 
-	const ButtonState DefaultOpenFavState(0, QObject::tr("Open (fav)"), QStringLiteral("Open favourite save location in new window"));
+	const ButtonState DefaultOpenFavState(0, QObject::tr("Open (fav)"), QObject::tr("Open favourite save location in new window"));
 
 
 	const QMap<unsigned short, ButtonState> DefaultStates {
