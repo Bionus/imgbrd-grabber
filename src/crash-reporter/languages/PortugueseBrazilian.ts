53b25f3e1dcb7d00af57c92e21608b71d84bbb5e
Jack Vasti <bio.nus@hotmail.fr>
2022-08-24 20:32:49 +0200

    New translations YourLanguage.ts (PortugueseBrazilian)

--- src/crash-reporter/languages/PortugueseBrazilian.ts
+++ src/crash-reporter/languages/PortugueseBrazilian.ts
@@ -4,52 +4,52 @@
   <context>
     <name>CrashReporterWindow</name>
     <message>
-      <location filename="../crash-reporter-window.ui" line="14"/>
+      <location filename="../src/crash-reporter-window.ui" line="14"/>
       <source>Crash Reporter</source>
       <translation>Relatório de falhas</translation>
     </message>
     <message>
-      <location filename="../crash-reporter-window.ui" line="28"/>
+      <location filename="../src/crash-reporter-window.ui" line="28"/>
       <source>Sorry</source>
       <translation>Sentimos muito</translation>
     </message>
     <message>
-      <location filename="../crash-reporter-window.ui" line="44"/>
+      <location filename="../src/crash-reporter-window.ui" line="44"/>
       <source>Grabber encountered a problem and crashed. The program will try to restore your tabs and other settings when it restarts.</source>
       <translation>Grabber encontrou um problema e travou. O programa tentará restaurar as abas e outras configurações quando for reiniciado.</translation>
     </message>
     <message>
-      <location filename="../crash-reporter-window.ui" line="60"/>
+      <location filename="../src/crash-reporter-window.ui" line="60"/>
       <source>To help us fix this crash, you can send us a bug report.</source>
       <translation>Para nos ajudar a corrigir o problema, você pode nos enviar um relatório do erro.</translation>
     </message>
     <message>
-      <location filename="../crash-reporter-window.ui" line="70"/>
+      <location filename="../src/crash-reporter-window.ui" line="70"/>
       <source>Send a bug report</source>
       <translation>Enviar relatório do erro</translation>
     </message>
     <message>
-      <location filename="../crash-reporter-window.ui" line="95"/>
+      <location filename="../src/crash-reporter-window.ui" line="95"/>
       <source>Log</source>
       <translation>Registro</translation>
     </message>
     <message>
-      <location filename="../crash-reporter-window.ui" line="102"/>
+      <location filename="../src/crash-reporter-window.ui" line="102"/>
       <source>Settings</source>
       <translation>Configurações</translation>
     </message>
     <message>
-      <location filename="../crash-reporter-window.ui" line="109"/>
+      <location filename="../src/crash-reporter-window.ui" line="109"/>
       <source>Dump</source>
       <translation>Despejo</translation>
     </message>
     <message>
-      <location filename="../crash-reporter-window.ui" line="155"/>
+      <location filename="../src/crash-reporter-window.ui" line="155"/>
       <source>Restart</source>
       <translation>Reiniciar</translation>
     </message>
     <message>
-      <location filename="../crash-reporter-window.ui" line="162"/>
+      <location filename="../src/crash-reporter-window.ui" line="162"/>
       <source>Quit</source>
       <translation>Sair</translation>
     </message>
