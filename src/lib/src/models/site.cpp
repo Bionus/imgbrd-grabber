f16d5722ed7d0221a12dcfb4e3c11f146e5591cf
Bionus <bio.nus@hotmail.fr>
2022-08-26 03:09:57 +0200

    Fix issue with loading cookies when there's only one entry

--- src/lib/src/models/site.cpp
+++ src/lib/src/models/site.cpp
@@ -128,7 +128,10 @@ void Site::loadConfig()
 
 	// Cookies
 	m_cookies.clear();
-	QList<QVariant> settingsCookies = m_settings->value("cookies").toList();
+	QVariant settingsCookiesVariant = m_settings->value("cookies");
+	QList<QVariant> settingsCookies = settingsCookiesVariant.type() == QVariant::String || settingsCookiesVariant.type() == QVariant::ByteArray
+		? QList<QVariant>{ settingsCookiesVariant }
+		: settingsCookiesVariant.toList();
 	for (const QVariant &variant : settingsCookies) {
 		QByteArray byteArray = variant.type() == QVariant::ByteArray ? variant.toByteArray() : variant.toString().toUtf8();
 		QList<QNetworkCookie> cookies = QNetworkCookie::parseCookies(byteArray);
