783c0a207fbc419200df1682866ca536c13cb551
Jack Vasti <bio.nus@hotmail.fr>
2022-10-16 15:34:11 +0200

    New translations YourLanguage.ts (ChineseSimplified)

--- src/crash-reporter/languages/ChineseSimplified.ts
+++ src/crash-reporter/languages/ChineseSimplified.ts
@@ -4,52 +4,52 @@
   <context>
     <name>CrashReporterWindow</name>
     <message>
-      <location filename="../crash-reporter-window.ui" line="14"/>
+      <location filename="../src/crash-reporter-window.ui" line="14"/>
       <source>Crash Reporter</source>
       <translation>错误报告</translation>
     </message>
     <message>
-      <location filename="../crash-reporter-window.ui" line="28"/>
+      <location filename="../src/crash-reporter-window.ui" line="28"/>
       <source>Sorry</source>
       <translation>抱歉！</translation>
     </message>
     <message>
-      <location filename="../crash-reporter-window.ui" line="44"/>
+      <location filename="../src/crash-reporter-window.ui" line="44"/>
       <source>Grabber encountered a problem and crashed. The program will try to restore your tabs and other settings when it restarts.</source>
       <translation>Grabber 遇到了问题并且停止工作。程序将会在重启时尝试恢复您的标签页与设置</translation>
     </message>
     <message>
-      <location filename="../crash-reporter-window.ui" line="60"/>
+      <location filename="../src/crash-reporter-window.ui" line="60"/>
       <source>To help us fix this crash, you can send us a bug report.</source>
       <translation>为了帮助我们修复问题，您可以给我们发送错误报告</translation>
     </message>
     <message>
-      <location filename="../crash-reporter-window.ui" line="70"/>
+      <location filename="../src/crash-reporter-window.ui" line="70"/>
       <source>Send a bug report</source>
       <translation>发送错误报告</translation>
     </message>
     <message>
-      <location filename="../crash-reporter-window.ui" line="95"/>
+      <location filename="../src/crash-reporter-window.ui" line="95"/>
       <source>Log</source>
       <translation>日志</translation>
     </message>
     <message>
-      <location filename="../crash-reporter-window.ui" line="102"/>
+      <location filename="../src/crash-reporter-window.ui" line="102"/>
       <source>Settings</source>
       <translation>设置</translation>
     </message>
     <message>
-      <location filename="../crash-reporter-window.ui" line="109"/>
+      <location filename="../src/crash-reporter-window.ui" line="109"/>
       <source>Dump</source>
       <translation>转储信息</translation>
     </message>
     <message>
-      <location filename="../crash-reporter-window.ui" line="155"/>
+      <location filename="../src/crash-reporter-window.ui" line="155"/>
       <source>Restart</source>
       <translation>重启程序</translation>
     </message>
     <message>
-      <location filename="../crash-reporter-window.ui" line="162"/>
+      <location filename="../src/crash-reporter-window.ui" line="162"/>
       <source>Quit</source>
       <translation>退出</translation>
     </message>
