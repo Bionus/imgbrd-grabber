3928ee2f9159676e25714e51a5d552be58fa1b77
Bionus <bio.nus@hotmail.fr>
2022-12-13 18:52:25 +0100

    fix: use auth ID as key instead of type in QML version

--- src/gui-qml/src/models/qml-auth.h
+++ src/gui-qml/src/models/qml-auth.h
@@ -12,22 +12,25 @@ class QmlAuth : public QObject
 {
 	Q_OBJECT
 
+	Q_PROPERTY(QString id READ id CONSTANT)
 	Q_PROPERTY(QString type READ type CONSTANT)
 	Q_PROPERTY(QList<QmlAuthSettingField*> fields READ fields CONSTANT)
 
 	public:
-		explicit QmlAuth(Auth *auth, QObject *parent = nullptr)
-			: QObject(parent), m_type(auth->type())
+		explicit QmlAuth(QString id, Auth *auth, QObject *parent = nullptr)
+			: QObject(parent), m_id(std::move(id)), m_type(auth->type())
 		{
 			for (const auto &field : auth->settingFields()) {
 				m_fields.append(new QmlAuthSettingField(field, this));
 			}
 		}
 
+		QString id() const { return m_id; }
 		QString type() const { return m_type; }
 		QList<QmlAuthSettingField*> fields() const { return m_fields; }
 
 	private:
+		QString m_id;
 		QString m_type;
 		QList<QmlAuthSettingField*> m_fields;
 };
