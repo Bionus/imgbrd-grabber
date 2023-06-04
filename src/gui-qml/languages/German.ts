ece5df2c3e2c2ec144c9ca13955b6bb4e4e9fa34
Jack Vasti <bio.nus@hotmail.fr>
2023-06-04 23:56:26 +0200

    New translations YourLanguage.ts (German)

--- src/gui-qml/languages/German.ts
+++ src/gui-qml/languages/German.ts
@@ -674,34 +674,6 @@
       <source>Tags</source>
       <translation>Tags</translation>
     </message>
-    <message>
-      <source>Artist</source>
-      <translation type="vanished">Künstler</translation>
-    </message>
-    <message>
-      <source>Copyright</source>
-      <translation type="vanished">Urheberrecht</translation>
-    </message>
-    <message>
-      <source>Character</source>
-      <translation type="vanished">Charakter</translation>
-    </message>
-    <message>
-      <source>Model</source>
-      <translation type="vanished">Modell</translation>
-    </message>
-    <message>
-      <source>Photo set</source>
-      <translation type="vanished">Fotosammlung</translation>
-    </message>
-    <message>
-      <source>Species</source>
-      <translation type="vanished">Arten</translation>
-    </message>
-    <message>
-      <source>Meta</source>
-      <translation type="vanished">Metadaten</translation>
-    </message>
   </context>
   <context>
     <name>SearchScreen</name>
@@ -970,42 +942,42 @@
     <message>
       <location filename="../src/main-screen.qml" line="47"/>
       <source>General</source>
-      <translation type="unfinished">Allgemein</translation>
+      <translation type="unfinished"/>
     </message>
     <message>
       <location filename="../src/main-screen.qml" line="48"/>
       <source>Artist</source>
-      <translation type="unfinished">Künstler</translation>
+      <translation type="unfinished"/>
     </message>
     <message>
       <location filename="../src/main-screen.qml" line="49"/>
       <source>Copyright</source>
-      <translation type="unfinished">Urheberrecht</translation>
+      <translation type="unfinished"/>
     </message>
     <message>
       <location filename="../src/main-screen.qml" line="50"/>
       <source>Character</source>
-      <translation type="unfinished">Charakter</translation>
+      <translation type="unfinished"/>
     </message>
     <message>
       <location filename="../src/main-screen.qml" line="51"/>
       <source>Model</source>
-      <translation type="unfinished">Modell</translation>
+      <translation type="unfinished"/>
     </message>
     <message>
       <location filename="../src/main-screen.qml" line="52"/>
       <source>Photo set</source>
-      <translation type="unfinished">Fotosammlung</translation>
+      <translation type="unfinished"/>
     </message>
     <message>
       <location filename="../src/main-screen.qml" line="53"/>
       <source>Species</source>
-      <translation type="unfinished">Arten</translation>
+      <translation type="unfinished"/>
     </message>
     <message>
       <location filename="../src/main-screen.qml" line="54"/>
       <source>Meta</source>
-      <translation type="unfinished">Metadaten</translation>
+      <translation type="unfinished"/>
     </message>
     <message>
       <location filename="../src/main-screen.qml" line="57"/>
