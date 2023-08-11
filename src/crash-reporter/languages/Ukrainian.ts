ea6fd16ec22898641cacf27508d56123eabe22f4
Jack Vasti <bio.nus@hotmail.fr>
2023-08-12 00:46:25 +0200

    New translations YourLanguage.ts (Ukrainian)

--- src/crash-reporter/languages/Ukrainian.ts
+++ src/crash-reporter/languages/Ukrainian.ts
@@ -6,52 +6,52 @@
     <message>
       <location filename="../src/crash-reporter-window.ui" line="14"/>
       <source>Crash Reporter</source>
-      <translation type="unfinished"/>
+      <translation>Звіт про Збій</translation>
     </message>
     <message>
       <location filename="../src/crash-reporter-window.ui" line="28"/>
       <source>Sorry</source>
-      <translation type="unfinished"/>
+      <translation>Вибачте</translation>
     </message>
     <message>
       <location filename="../src/crash-reporter-window.ui" line="44"/>
       <source>Grabber encountered a problem and crashed. The program will try to restore your tabs and other settings when it restarts.</source>
-      <translation type="unfinished"/>
+      <translation>Grabber зіштовхнувся з помилкою і розбився. Програма спробує відновити ваші вкладки та інші устави після перезапуску.</translation>
     </message>
     <message>
       <location filename="../src/crash-reporter-window.ui" line="60"/>
       <source>To help us fix this crash, you can send us a bug report.</source>
-      <translation type="unfinished"/>
+      <translation>Аби допомогти нам із цим збоєм, ви можете надіслати нам звіт про помилку.</translation>
     </message>
     <message>
       <location filename="../src/crash-reporter-window.ui" line="70"/>
       <source>Send a bug report</source>
-      <translation type="unfinished"/>
+      <translation>Надіслати звіт про помилку</translation>
     </message>
     <message>
       <location filename="../src/crash-reporter-window.ui" line="95"/>
       <source>Log</source>
-      <translation type="unfinished"/>
+      <translation>Звіт</translation>
     </message>
     <message>
       <location filename="../src/crash-reporter-window.ui" line="102"/>
       <source>Settings</source>
-      <translation type="unfinished"/>
+      <translation>Устави</translation>
     </message>
     <message>
       <location filename="../src/crash-reporter-window.ui" line="109"/>
       <source>Dump</source>
-      <translation type="unfinished"/>
+      <translation>Дамп</translation>
     </message>
     <message>
       <location filename="../src/crash-reporter-window.ui" line="155"/>
       <source>Restart</source>
-      <translation type="unfinished"/>
+      <translation>Перезапуск</translation>
     </message>
     <message>
       <location filename="../src/crash-reporter-window.ui" line="162"/>
       <source>Quit</source>
-      <translation type="unfinished"/>
+      <translation>Вийти</translation>
     </message>
   </context>
 </TS>
