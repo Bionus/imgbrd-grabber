c48b7c47958548e7eb83632816f4c0d9495e8a88
Bionus <bio.nus@hotmail.fr>
2023-11-22 22:34:44 +0100

    build: fix Android build on Qt 6.6.0

--- src/dist/android/src/org/bionus/grabber/ShareUtils.java
+++ src/dist/android/src/org/bionus/grabber/ShareUtils.java
@@ -8,7 +8,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.util.Log;
 
-import android.support.v4.content.FileProvider;
+import androidx.core.content.FileProvider;
 
 public class ShareUtils
 {
