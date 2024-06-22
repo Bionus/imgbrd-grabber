96e30a0712b559c43b5c71ff4581059a7b323583
ProtagNeptune <93203147+ProtagNeptune@users.noreply.github.com>
2024-06-22 09:09:23 +0200

    Update Japanese.ts

--- src/languages/Japanese.ts
+++ src/languages/Japanese.ts
@@ -1519,17 +1519,17 @@ Please solve the issue before resuming the download.</source>
     </message>
     <message>
       <location filename="../gui/src/tabs/log-tab.ui" line="71"/>
-      <source>Clear log</source>
+      <source>Clear log file</source>
       <translation>ログを消去</translation>
     </message>
     <message>
       <location filename="../gui/src/tabs/log-tab.ui" line="78"/>
-      <source>Open log</source>
+      <source>Open log file</source>
       <translation>ログを開く</translation>
     </message>
     <message>
       <location filename="../gui/src/tabs/log-tab.ui" line="85"/>
-      <source>Open settings directory</source>
+      <source>Open log folder</source>
       <translation>設定フォルダーを開く</translation>
     </message>
   </context>
