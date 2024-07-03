c816e774f07d562a46bc492236c3206e5c15c3f3
Jack Vasti <bio.nus@hotmail.fr>
2024-07-03 19:53:02 +0200

    New translations yourlanguage.ts (PortugueseBrazilian)

--- src/gui-qml/languages/PortugueseBrazilian.ts
+++ src/gui-qml/languages/PortugueseBrazilian.ts
@@ -555,7 +555,7 @@
     <message>
       <location filename="../src/components/MainDrawer.qml" line="118"/>
       <source>Sources</source>
-      <translation type="unfinished"/>
+      <translation>Fontes</translation>
     </message>
     <message>
       <location filename="../src/components/MainDrawer.qml" line="124"/>
@@ -701,7 +701,7 @@
     <message>
       <location filename="../src/components/settings/pages/SaveSettingsPage.qml" line="79"/>
       <source>Artist</source>
-      <translation type="unfinished"/>
+      <translation>Artista</translation>
     </message>
     <message>
       <location filename="../src/components/settings/pages/SaveSettingsPage.qml" line="87"/>
