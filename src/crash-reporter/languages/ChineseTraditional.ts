5635f1c7527f766d3280430f9f6de6262ef224af
Jack Vasti <bio.nus@hotmail.fr>
2025-10-22 23:09:46 +0200

    New translations yourlanguage.ts (ChineseTraditional)

--- src/crash-reporter/languages/ChineseTraditional.ts
+++ src/crash-reporter/languages/ChineseTraditional.ts
@@ -6,52 +6,52 @@
     <message>
       <location filename="../src/crash-reporter-window.ui" line="14"/>
       <source>Crash Reporter</source>
-      <translation>錯誤報告</translation>
+      <translation type="unfinished"/>
     </message>
     <message>
       <location filename="../src/crash-reporter-window.ui" line="28"/>
       <source>Sorry</source>
-      <translation>抱歉！</translation>
+      <translation type="unfinished"/>
     </message>
     <message>
       <location filename="../src/crash-reporter-window.ui" line="44"/>
       <source>Grabber encountered a problem and crashed. The program will try to restore your tabs and other settings when it restarts.</source>
-      <translation>Grabber 遇到了問題並且停止工作。程序將會在重啟時嘗試恢復您的標籤頁與設置</translation>
+      <translation type="unfinished"/>
     </message>
     <message>
       <location filename="../src/crash-reporter-window.ui" line="60"/>
       <source>To help us fix this crash, you can send us a bug report.</source>
-      <translation>為了幫助我們修復問題，您可以給我們發送錯誤報告</translation>
+      <translation type="unfinished"/>
     </message>
     <message>
       <location filename="../src/crash-reporter-window.ui" line="70"/>
       <source>Send a bug report</source>
-      <translation>發送錯誤報告</translation>
+      <translation type="unfinished"/>
     </message>
     <message>
       <location filename="../src/crash-reporter-window.ui" line="95"/>
       <source>Log</source>
-      <translation>日誌</translation>
+      <translation type="unfinished"/>
     </message>
     <message>
       <location filename="../src/crash-reporter-window.ui" line="102"/>
       <source>Settings</source>
-      <translation>設置</translation>
+      <translation type="unfinished"/>
     </message>
     <message>
       <location filename="../src/crash-reporter-window.ui" line="109"/>
       <source>Dump</source>
-      <translation>轉儲資訊</translation>
+      <translation type="unfinished"/>
     </message>
     <message>
       <location filename="../src/crash-reporter-window.ui" line="155"/>
       <source>Restart</source>
-      <translation>重啟程序</translation>
+      <translation type="unfinished"/>
     </message>
     <message>
       <location filename="../src/crash-reporter-window.ui" line="162"/>
       <source>Quit</source>
-      <translation>退出</translation>
+      <translation type="unfinished"/>
     </message>
   </context>
 </TS>
