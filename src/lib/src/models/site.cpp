f462ebf1e877b8d6f896d69e23ccc5d8034d56c3
Bionus <bio.nus@hotmail.fr>
2026-07-30 21:56:34 +0200

    chore: bump User-Agent version

--- src/lib/src/models/site.cpp
+++ src/lib/src/models/site.cpp
@@ -163,7 +163,7 @@ void Site::loadConfig()
 	// Generate the User-Agent
 	m_userAgent = m_settings->value("Headers/User-Agent").toString();
 	if (m_userAgent.isEmpty()) {
-		const QString globalUserAgent = pSettings->value("userAgent", QStringLiteral("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:108.0) Gecko/20100101 Firefox/108.0")).toString();
+		const QString globalUserAgent = pSettings->value("userAgent", QStringLiteral("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:153.0) Gecko/20100101 Firefox/153.0")).toString();
 		#if defined(USE_WEBENGINE)
 			const bool defaultUseQtUserAgent = true;
 		#else
